56177e88372aebafa3a0b506cd3cc9e8003d46e1
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-15 17:26:06 -0800

    feat(backend): create db.ts for db pool
    Created db.ts to bridge db to backend

---
+++ server/db.ts
@@ -0,0 +1,28 @@
+import Pool from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+export const query = async (
+  text: string,
+  params?: (string | number | boolean | null)[]
+) => {
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(text, params);
+    return result;
+  } catch (err) {
+    console.error('Database query error:', err);
+    throw err;
+  } finally {
+    client.release();
+  }
+};
+
+export default pool;
