37defa0d82f96ae275f656bdfe3c55152fda248c
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-16 16:29:37 -0800

    feat(backend): create get route for users
    Created route folder with userRoutes; implemented findMany feature using Prisma; called route in index.ts.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,43 @@
-import express, { Request, Response } from 'express';
-const app = express();
-const port = 3000;
+import express, { Request, Response, Application } from 'express';
+import userRoutes from './routes/userRoutes';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app: Application = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use('/api', userRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send(
-    'Hello! This is your first app with Express.js, Typescript, and Nodemon, AND browser-sync!'
+    'Hello! This is your first app with Express.js, Typescript, and Nodemon!'
   );
 });
 
-app.listen(port, () => {
-  console.log(`Server is Running on port ${port} with Nodemon!`);
+// get all users
+app.get('/', (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+  } catch (error) {
+    console.error(error);
+  }
+});
+// get all projects
+
+// get all boards
+
+// get all tickets
+
+// get all comments
+
+// get all attachments
+
+// get all labels
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is Running on port ${process.env.PORT} with Nodemon!`);
 });
---
+++ server/src/routes/userRoutes.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const router = Router();
+const prisma = new PrismaClient();
+
+router.get('/users', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error fetching users: ', error);
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+export default router;
