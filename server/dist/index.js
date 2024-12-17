f3228c42156e0927f5a770e6d4e148eb591a81ee
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-16 16:29:37 -0800

    feat(backend): create get route for users
    Created route folder with userRoutes; implemented findMany feature using Prisma; called route in index.ts.

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,11 +4,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
+// Middleware
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use('/api', userRoutes_1.default);
 app.get('/', (req, res) => {
-    res.send('Hello! This is your first app with Express.js, Typescript, and Nodemon, AND browser-sync!');
+    res.send('Hello! This is your first app with Express.js, Typescript, and Nodemon!');
 });
-app.listen(port, () => {
-    console.log(`Server is Running on port ${port} with Nodemon!`);
+// get all users
+app.get('/', (req, res) => {
+    try {
+        console.log(req.body);
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+// get all projects
+// get all boards
+// get all tickets
+// get all comments
+// get all attachments
+// get all labels
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running on port ${process.env.PORT} with Nodemon!`);
 });
