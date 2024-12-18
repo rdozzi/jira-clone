2d50fd07617e49a4ad2040ff519d4c044cb89a90
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-18 12:43:11 -0800

    feat(backend): :art: add project and ticket route files; add calls to  main entry file
    Added project and ticket routes to my routes folder; Added basic get calls to that file.

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
+const ticketRoutes_1 = __importDefault(require("./routes/ticketRoutes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
@@ -12,22 +14,16 @@ const app = (0, express_1.default)();
 // Middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/api', userRoutes_1.default);
 app.get('/', (req, res) => {
     res.send('Hello! This is your first app with Express.js, Typescript, and Nodemon!');
 });
 // get all users
-app.get('/', (req, res) => {
-    try {
-        console.log(req.body);
-    }
-    catch (error) {
-        console.error(error);
-    }
-});
+app.use('/api', userRoutes_1.default);
 // get all projects
-// get all boards
+app.use('/api', projectRoutes_1.default);
 // get all tickets
+app.use('/api', ticketRoutes_1.default);
+// get all boards
 // get all comments
 // get all attachments
 // get all labels
---
+++ server/dist/routes/projectRoutes.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const client_1 = require("@prisma/client");
+const router = (0, express_1.Router)();
+const prisma = new client_1.PrismaClient();
+router.get('/projects', async (req, res) => {
+    try {
+        const projects = await prisma.project.findMany();
+        res.status(200).json(projects);
+    }
+    catch (error) {
+        console.error('Error fetching projects: ', error);
+        res.status(500).json({ error: 'Failed to fetch projects' });
+    }
+});
+exports.default = router;
