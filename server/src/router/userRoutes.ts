9f861d7770629fafd5795d49f6642e6fa203223a
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-18 20:44:45 -0800

    chore: change routes to router
    Changed the routes folder  nameto the router.

--- server/src/router/userRoutes.ts
+++ server/src/router/userRoutes.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 const router = Router();
 const prisma = new PrismaClient();
 
+// Get all users
 router.get('/users', async (req: Request, res: Response): Promise<void> => {
   try {
     const users = await prisma.user.findMany();
@@ -14,6 +15,8 @@ router.get('/users', async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+// Get user by Id
+
 router.get('/users/:id', async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
   try {
@@ -22,6 +25,19 @@ router.get('/users/:id', async (req: Request, res: Response): Promise<void> => {
   } catch (error) {
     console.error('Error fetching users: ', error);
     res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+// Create user
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userDetails = await req.body;
+    const user = await prisma.user.create({ data: userDetails });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Error creating user: ', error);
+    res.status(500).json({ error: 'Failed to create user' });
   }
 });
 
