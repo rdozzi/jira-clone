73beb7d43581b3b7376c1fd6a3abb9228ce276c7
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-18 21:01:01 -0800

    feat(backend): add deleteUserById function
    Added a deleteUserById function. Tested route with Postman.

--- server/src/router/userRoutes.ts
+++ server/src/router/userRoutes.ts
@@ -20,8 +20,8 @@ router.get('/users', async (req: Request, res: Response): Promise<void> => {
 router.get('/users/:id', async (req: Request, res: Response): Promise<void> => {
   const { id } = req.params;
   try {
-    const users = await prisma.user.findUnique({ where: { id: Number(id) } });
-    res.status(200).json(users);
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+    res.status(200).json(user);
   } catch (error) {
     console.error('Error fetching users: ', error);
     res.status(500).json({ error: 'Failed to fetch users' });
@@ -40,5 +40,21 @@ router.post('/', async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: 'Failed to create user' });
   }
 });
+
+router.delete(
+  '/users/:id',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const deleteData = await prisma.user.delete({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(deleteData);
+    } catch (error) {
+      console.error('Error fetching users: ', error);
+      res.status(500).json({ error: 'Failed to fetch users' });
+    }
+  }
+);
 
 export default router;
