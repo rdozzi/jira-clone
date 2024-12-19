17a09d5bcb09c30bbb23a662a4ded6091dbd8d02
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-19 10:18:58 -0800

    feat(backend): add create and delete ops for projects
    Added create and delete operations for projects.

--- server/dist/router/projectRoutes.js
+++ server/dist/router/projectRoutes.js
@@ -27,4 +27,30 @@ router.get('/projects/:id', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch projects' });
     }
 });
+router.post('/projects', async (req, res) => {
+    try {
+        const projectData = req.body;
+        const project = await prisma.project.create({
+            data: projectData,
+        });
+        res.status(200).json(project);
+    }
+    catch (error) {
+        console.error('Error creating project: ', error);
+        res.status(500).json({ error: 'Failed to create project' });
+    }
+});
+router.delete('/projects/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteProject = await prisma.project.delete({
+            where: { id: Number(id) },
+        });
+        res.status(200).json(deleteProject);
+    }
+    catch (error) {
+        console.error('Error fetching project: ', error);
+        res.status(500).json({ error: 'Failed to fetch project' });
+    }
+});
 exports.default = router;
--- server/src/router/projectRoutes.ts
+++ server/src/router/projectRoutes.ts
@@ -30,4 +30,33 @@ router.get(
   }
 );
 
+router.post('/projects', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const projectData = req.body;
+    const project = await prisma.project.create({
+      data: projectData,
+    });
+    res.status(200).json(project);
+  } catch (error) {
+    console.error('Error creating project: ', error);
+    res.status(500).json({ error: 'Failed to create project' });
+  }
+});
+
+router.delete(
+  '/projects/:id',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const deleteProject = await prisma.project.delete({
+        where: { id: Number(id) },
+      });
+      res.status(200).json(deleteProject);
+    } catch (error) {
+      console.error('Error fetching project: ', error);
+      res.status(500).json({ error: 'Failed to fetch project' });
+    }
+  }
+);
+
 export default router;
