ab8c15e840af93504041fb99596f82a0075d5b2f
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-19 23:32:45 -0800

    feat(backend): add get and create features for comments
    Added get and create operation features for comments

---
+++ server/dist/router/commentRoutes.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const client_1 = require("@prisma/client");
+const router = (0, express_1.Router)();
+const prisma = new client_1.PrismaClient();
+// Get all comments
+router.get('/comments', async (req, res) => {
+    try {
+        const comments = await prisma.comment.findMany();
+        res.status(200).json(comments);
+    }
+    catch (error) {
+        console.error('Error fetching comments: ', error);
+        res.status(500).json({ error: 'Failed to fetch comments' });
+    }
+});
+// Get comments for a specific ticket
+router.get('/tickets/:ticketId/comments', async (req, res) => {
+    const { ticketId } = req.params;
+    try {
+        const ticketComments = await prisma.comment.findMany({
+            where: { ticketId: Number(ticketId) },
+        });
+        res.status(200).json(ticketComments);
+    }
+    catch (error) {
+        console.error('Error fetching comments: ', error);
+        res.status(500).json({ error: 'Failed to fetch comments' });
+    }
+});
+// Create a comment
+router.post('/comments', async (req, res) => {
+    try {
+        const commentData = req.body;
+        const comment = await prisma.comment.create({
+            data: commentData,
+        });
+        res.status(200).json(comment);
+    }
+    catch (error) {
+        console.error('Error creating comment: ', error);
+        res.status(500).json({ error: 'Failed to create comment' });
+    }
+});
+exports.default = router;
