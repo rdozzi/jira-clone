3f6abe70f4e8efaf752d2924e4d7cce9680fd04b
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-21 14:11:33 -0800

    feat(frontend): add feature to render client names on webpage.
    Added state and effect hooks to call data from database to appear on the front end. This will act as a template to implement all of the current routes present in the backend.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+
+interface User {
+  id: string;
+  name: string;
+}
+
 function App() {
-  return <div>Hello! This is my Vite App</div>;
+  const [userData, setUserData] = useState<User[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await fetch('http://localhost:3000/api/users');
+        if (!res.ok) {
+          throw new Error('Failed to fetch users');
+        }
+        const data = await res.json();
+        setUserData(data);
+      } catch (err: any | unknown) {
+        console.error(err);
+        setError(err.message);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      {error && <p>Error: {error}</p>}
+      <ul>
+        {userData.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
