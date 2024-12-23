2977949ccfdc7265d37508a54a8c8fc79d396adb
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-22 17:19:18 -0800

    feat(frontend): create user command components and rendered in main
    Created components with logic for CRUD operations (create, delete, get all/by id) for user.

---
+++ client/src/CreateUser.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+
+function CreateUser() {
+  const [formData, setFormData] = useState({
+    name: 'Judith',
+    email: 'judith@example.com',
+    passwordHash: 'password!556',
+    role: 'USER',
+  });
+
+  function handleChange(e: any) {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
+
+  async function handleSubmit(e: any): Promise<void> {
+    e.preventDefault();
+    try {
+      const res = await fetch('http://localhost:3000/api/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      if (!res.ok) {
+        throw new Error('Failed to create user');
+      }
+
+      const result = await res.json();
+      console.log('User created successfully:', result);
+    } catch (error) {
+      console.error('Error create user:', error);
+    }
+  }
+
+  return (
+    <>
+      <form method='post' onSubmit={handleSubmit}>
+        <br />
+        <label>Name: </label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          defaultValue={formData.name}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Email: </label>
+        <input
+          type='text'
+          id='email'
+          name='email'
+          defaultValue={formData.email}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Password: </label>
+        <input
+          type='text'
+          id='password'
+          name='passwordHash'
+          defaultValue={formData.passwordHash}
+          onChange={handleChange}
+        />
+        <br />
+        <label>Role: </label>
+        <input
+          type='text'
+          id='role'
+          name='role'
+          defaultValue={formData.role}
+          onChange={handleChange}
+        />
+        <br />
+
+        <button type='submit'>Create A User</button>
+      </form>
+    </>
+  );
+}
+
+export default CreateUser;
---
+++ client/src/DeleteUser.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+function DeleteUser() {
+  const [id, setId] = useState<number | string>('');
+
+  function handleChange(e: any) {
+    e.preventDefault();
+    const { value } = e.target;
+    setId(Number(value));
+  }
+
+  async function handleSubmit(e: any): Promise<void> {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/users/${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (!res.ok) {
+        throw new Error('Failed to Delete User');
+      }
+
+      const result = await res.json();
+      console.log('User delted successfully:', result);
+    } catch (error) {
+      console.error('Could not delete user:', error);
+    }
+  }
+
+  return (
+    <form method='delete' onSubmit={handleSubmit}>
+      <br />
+      <div>Provide the number of the user you want to delete</div>
+      <label>Id Number: </label>
+      <input
+        type='number'
+        id='userId'
+        name='userId'
+        defaultValue={id}
+        onChange={handleChange}
+      />
+      <br />
+      <button type='submit'>Delete User</button>
+    </form>
+  );
+}
+
+export default DeleteUser;
