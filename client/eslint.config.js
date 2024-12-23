ca561d8daece66ed32e840a44828bf6526f5b9d4
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-22 17:17:46 -0800

    chore: add @typescript-eslint/no-unused-vars rule to eslit.config.js; add forceConsistentCasingInFileNames to tsconfig
    changed @typescript-eslint/no-unused-vars rule to 'warn' to prevent application termination. Added forceConsistentCasingInFileNames to compilerOptions and changed to false to prevent application termination.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -26,6 +26,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   }
 );
