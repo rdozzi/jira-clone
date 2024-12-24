ad6f4ad49cd8d0998cd4c96cc23dca3a6cebf576
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-23 17:31:51 -0800

    feat(frontend): add GetTicketByUserId component
    Created a GetTicketsByUserIDComponent to render user specific ticket information to browser. Integrated component to main.tsx.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,14 @@ import GetAllUsers from './GetAllUsers';
 import GetUserById from './GetUserById';
 import CreateUser from './CreateUser';
 import DeleteUser from './DeleteUser';
+import GetTicketsByUserId from './GetTicketsByUserId';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GetAllUsers />
-    <GetUserById />
-    <CreateUser />
-    <DeleteUser />
+    {/* <GetAllUsers /> */}
+    {/* <GetUserById /> */}
+    {/* <CreateUser /> */}
+    {/* <DeleteUser /> */}
+    <GetTicketsByUserId />
   </StrictMode>
 );
