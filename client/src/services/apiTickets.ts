02fa393a764a39f21e2e1f0eb22c3edcf3eb6044
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-25 20:44:47 -0800

    feat(frontend): create services folder with apiTickets file
    Created a services folder and added apiTickets for future api calls to backed pertaining to tickets.

---
+++ client/src/services/apiTickets.ts
@@ -0,0 +1,22 @@
+interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  type: string;
+}
+
+export async function getTickets() {
+  try {
+    const res = await fetch('http://localhost:3000/api/tickets');
+    if (!res.ok) {
+      throw new Error('Failed to fetch tickets');
+    }
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
