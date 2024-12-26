6ab7461e9ab493ed3d392dcab2035bfb72e34794
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-25 19:08:49 -0800

    feat(frontend): add multiple sort table template with ticket columns.

--- client/src/pages/TicketList.tsx
+++ client/src/pages/TicketList.tsx
@@ -1,5 +1,52 @@
-function TicketList() {
-  return <div>TicketList!</div>;
+import React from 'react';
+import { Table } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
+
+interface DataType {
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  priority: string;
+  type: string;
 }
 
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    sorter: { compare: (a, b) => a.title - b.title },
+  },
+  {
+    title: 'Description',
+    dataIndex: 'description',
+    sorter: { compare: (a, b) => a.description - b.description },
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    sorter: { compare: (a, b) => a.status - b.status },
+  },
+  {
+    title: 'Priority',
+    dataIndex: 'priority',
+    sorter: { compare: (a, b) => a.priority - b.priority },
+  },
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    sorter: { compare: (a, b) => a.priority - b.priority },
+  },
+];
+
+const onChange: TableProps<DataType>['onChange'] = (
+  pagination,
+  filters,
+  sorter,
+  extra
+) => console.log('params', pagination, filters, sorter, extra);
+
+function TicketList(): React.FC {
+  return <Table<DataType> columns={columns} onChange={onChange} />;
+}
 export default TicketList;
