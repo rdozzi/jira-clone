2a06c10ad519d6dd68279c4f27559b2b63460a75
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-25 16:06:47 -0800

    feat(frontend): add Sidebar, Header, Footer, and Content to AppLayout
    Added starter Header, Sider, Footer and Content sections to AppLayout with basic styling

--- client/src/ui/AppLayout.tsx
+++ client/src/ui/AppLayout.tsx
@@ -1,5 +1,34 @@
-function AppLayout({ children }) {
-  return <div>AppLayout!</div>;
+import { Outlet } from 'react-router-dom';
+import { Flex, Layout } from 'antd';
+
+const { Header, Sider, Footer, Content } = Layout;
+
+type StyleObject = React.CSSProperties;
+
+const siderStyle: StyleObject = { backgroundColor: 'aqua', height: '100vh' };
+
+const headerStyle: StyleObject = {
+  backgroundColor: 'blue',
+  color: 'white',
+};
+
+const footerStyle: StyleObject = {
+  backgroundColor: 'lightblue',
+};
+
+function AppLayout() {
+  return (
+    <Layout>
+      <Sider style={siderStyle}>Sider</Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default AppLayout;
