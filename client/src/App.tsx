c226ebe501febbb15129f548eda8431d55150e45
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-26 09:37:04 -0800

    feat(frontend): implement QueryClient, QueryClientProvider, and ReactQueryDevtools into App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,33 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AppLayout from './ui/AppLayout';
 import TicketList from './pages/TicketList';
 import KanbanBoard from './pages/KanbanBoard';
 import Calender from './pages/Calender';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path='tickets/ticketlist' element={<TicketList />} />
-          <Route path='tickets/kanbanboard' element={<KanbanBoard />} />
-          <Route path='tickets/calendar' element={<Calender />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path='tickets/ticketlist' element={<TicketList />} />
+            <Route path='tickets/kanbanboard' element={<KanbanBoard />} />
+            <Route path='tickets/calendar' element={<Calender />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
