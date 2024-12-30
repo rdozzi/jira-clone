62a1466a99ab673e15817144cbea68fbed149916
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-30 11:52:27 -0800

    feat(frontend): render initial calendar from antd website

--- client/src/pages/Calendar.tsx
+++ client/src/pages/Calendar.tsx
@@ -1,5 +1,13 @@
+import { Calendar } from 'antd';
+import type { CalendarProps } from 'antd';
+import type { Dayjs } from 'dayjs';
+
 function Calender() {
-  return <div>Calendar!!</div>;
+  const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  };
+
+  return <Calendar onPanelChange={onPanelChange} />;
 }
 
 export default Calender;
