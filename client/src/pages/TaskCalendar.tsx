10c119c3a38858045d4129766f63a4ed544f280f
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-30 13:38:29 -0800

    feat(frontend): add Badges to dates with events
    Added badges with static data to provide to calendar

--- client/src/pages/TaskCalendar.tsx
+++ client/src/pages/TaskCalendar.tsx
@@ -1,13 +1,32 @@
-import { Calendar } from 'antd';
-import type { CalendarProps } from 'antd';
-import type { Dayjs } from 'dayjs';
+import { useState } from 'react';
+import { Calendar, Badge } from 'antd';
+import dayjs from 'dayjs';
+
+const events = [
+  { id: 1, title: 'Ticket1', date: '2024-12-30', status: 'Completed' },
+  { id: 2, title: 'Ticket2', date: '2024-12-31', status: 'In Progress' },
+];
 
 function TaskCalender() {
-  const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
-    console.log(value.format('YYYY-MM-DD'), mode);
-  };
+  function cellRender(date, info) {
+    if (info.type === 'date') {
+      const formattedDate = dayjs(date).format('YYYY-MM-DD');
+      console.log(formattedDate);
+      const dayEvents = events.filter((event) => event.date === formattedDate);
 
-  return <Calendar onPanelChange={onPanelChange} />;
+      return (
+        <ul>
+          {dayEvents.map((event) => (
+            <li key={event.id}>
+              <Badge status='success' text={event.title} />
+            </li>
+          ))}
+        </ul>
+      );
+    }
+    return null;
+  }
+
+  return <Calendar cellRender={cellRender} />;
 }
-
 export default TaskCalender;
