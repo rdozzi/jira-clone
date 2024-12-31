62ba4fdfe697e4e40d2e5c807fefe5483ae9b09d
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-31 10:55:12 -0800

    feat(frontend): add state for query call
    Added useState and useEffect similar to boards for query call

--- client/src/pages/TaskCalendar.tsx
+++ client/src/pages/TaskCalendar.tsx
@@ -1,26 +1,41 @@
+import { useState, useEffect } from 'react';
 import { useGetTickets } from '../features/tickets/useGetTickets';
 import { Calendar, Badge } from 'antd';
 import dayjs from 'dayjs';
 
-const events = [
-  { id: 1, title: 'Ticket1', date: '2024-12-30', status: 'Completed' },
-  { id: 2, title: 'Ticket2', date: '2024-12-31', status: 'In Progress' },
-];
+// const events = [
+//   { id: 1, title: 'Ticket1', date: '2024-12-30', status: 'Completed' },
+//   { id: 2, title: 'Ticket2', date: '2024-12-31', status: 'In Progress' },
+// ];
 
 function TaskCalender() {
   const { isLoading, tickets, error } = useGetTickets();
+  const [ticketState, setTicketState] = useState([]);
+  useEffect(() => {
+    if (tickets) {
+      setTicketState(() => tickets);
+    }
+  }, [tickets]);
+
+  console.log(ticketState);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   function cellRender(date, info) {
     if (info.type === 'date') {
       const formattedDate = dayjs(date).format('YYYY-MM-DD');
-      console.log(formattedDate);
-      const dayEvents = events.filter((event) => event.date === formattedDate);
+      const activeTickets = ticketState.filter(
+        (ticket) => dayjs(ticket.dueDate).format('YYYY-MM-DD') === formattedDate
+      );
+      console.log(activeTickets);
 
       return (
         <ul>
-          {dayEvents.map((tickets) => (
-            <li key={tickets.id}>
-              <Badge status='success' text={tickets.title} />
+          {activeTickets.map((activeTicket) => (
+            <li key={activeTicket.id}>
+              <Badge status='success' text={activeTicket.title} />
             </li>
           ))}
         </ul>
