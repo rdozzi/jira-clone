18dea8c8ec22007cfea65cf461f2edd662d50326
Raphael Dozzi <rdozzi84@gmail.com>
2024-12-30 17:59:15 -0800

    chore(database): :zap: fix message typo for ticket route

--- server/src/router/ticketRoutes.ts
+++ server/src/router/ticketRoutes.ts
@@ -10,8 +10,8 @@ router.get('/tickets', async (req: Request, res: Response): Promise<void> => {
     const tickets = await prisma.ticket.findMany();
     res.status(200).json(tickets);
   } catch (error) {
-    console.error('Error fetching users: ', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
+    console.error('Error fetching tickets: ', error);
+    res.status(500).json({ error: 'Failed to fetch tickets' });
   }
 });
 
