20ee963828a860dd59d5225f27d19e9e13efb353
Raphael Dozzi <rdozzi84@gmail.com>
2025-01-01 20:17:34 -0800

    feat(frontend): Add antd ConfigProvider component to App.tsx
    Added antd ConfigProvider to App.tsx and wrapped all components below BrowserRouter with it. Changed the Tabs itemColor to white, #ffffff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ConfigProvider } from 'antd';
 import AppLayout from './ui/AppLayout';
 import TicketList from './pages/TicketList';
 import TaskCalendar from './pages/TaskCalendar';
@@ -18,15 +19,20 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path='tickets/ticketlist' element={<TicketList />} />
-            <Route path='tickets/taskboard' element={<TaskBoard />} />
-            <Route path='tickets/calendar' element={<TaskCalendar />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
+      <ConfigProvider
+        theme={{ components: { Tabs: { itemColor: '#ffffff' } } }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path='tickets/ticketlist' element={<TicketList />} />
+              <Route path='tickets/taskboard' element={<TaskBoard />} />
+              <Route path='tickets/calendar' element={<TaskCalendar />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
     </QueryClientProvider>
   );
 }
