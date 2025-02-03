d36ad9a58896a9f93cf87c6d40ae83d833b239cb
Raphael Dozzi <rdozzi84@gmail.com>
2025-02-03 09:59:21 -0800

    feat(frontend): create api function createTicket in apiTickets
    Created a function createTicket to process post requests for new tickets.

--- client/src/services/apiTickets.ts
+++ client/src/services/apiTickets.ts
@@ -19,3 +19,22 @@ export async function getTickets() {
     console.error(err);
   }
 }
+
+export async function createTicket(ticket: Ticket) {
+  try {
+    const res = await fetch('http://localhost:3000/api/tickets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(ticket),
+    });
+    if (!res.ok) {
+      throw new Error('Failed to create ticket');
+    }
+    const data = await res.json();
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
