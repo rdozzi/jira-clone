de5749dc3b5cc019045a5217d5c78a94b7a518f0
Raphael Dozzi <rdozzi84@gmail.com>
2025-02-15 07:53:14 -0800

    feat(frontend): update ViewEditTicketModal component
    Added props to component for development.

--- client/src/ui/ViewEditTicketModal.tsx
+++ client/src/ui/ViewEditTicketModal.tsx
@@ -1,6 +1,24 @@
-import { Modal } from 'antd';
+import { Modal, Input } from 'antd';
 
 import { Record } from './TicketListItemButton';
+
+import { useGetUsers } from '../features/users/useGetUsers';
+
+// export interface Record {
+//   assignee: { first_name: string; last_name: string };
+//   assigneeId: number;
+//   boardId: number;
+//   createdAt: string;
+//   description: string;
+//   dueDate: string;
+//   id: number;
+//   priority: string;
+//   reporterId: number;
+//   status: string;
+//   title: string;
+//   type: string;
+//   udpatedAt: string;
+// }
 
 function ViewEditTicketModal({
   closeModal,
@@ -9,16 +27,15 @@ function ViewEditTicketModal({
   closeModal: () => void;
   record: Record;
 }) {
+  const { title, description, dueDate, status, priority, type } = record;
   return (
     <Modal
       title='View/Edit Ticket'
       open={true}
       onCancel={closeModal}
-      footer={null}
-    >
-      <p>Ticket Title: {record.title}</p>
-      <p>Ticket Description: {record.description}</p>
-    </Modal>
+      onOk={closeModal}
+      mask={false}
+    ></Modal>
   );
 }
 
