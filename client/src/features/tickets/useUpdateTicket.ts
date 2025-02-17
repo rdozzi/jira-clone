56ced80a8412d2710ed226b8e50fe7e5cabfbff1
Raphael Dozzi <rdozzi84@gmail.com>
2025-02-16 18:33:03 -0800

    feat(frontend): update mutation function with return statement for apiUpdateTicket call; add onError function for error communication.

--- client/src/features/tickets/useUpdateTicket.ts
+++ client/src/features/tickets/useUpdateTicket.ts
@@ -1,14 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateTicket as apiUpdateTicket } from '../../services/apiTickets';
 
-export function useUpdateTicket(ticketId: any, values: any) {
+export function useUpdateTicket() {
   const queryClient = useQueryClient();
 
   const { mutate: updateTicket, status } = useMutation({
-    mutationFn: ({ ticketId, values }: { ticketId: number; values: any }) =>
-      apiUpdateTicket(ticketId, values),
+    mutationFn: ({ ticketId, values }: { ticketId: number; values: any }) => {
+      return apiUpdateTicket(ticketId, values);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['tickets'] });
+    },
+    onError: (error) => {
+      console.error('Mutation Failed:', error);
     },
   });
 
