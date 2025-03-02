9455286605a7350a0b7d1b4b336a00b0b0f25de2
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-01 18:02:03 -0800

    feat(frontend): create ThemeProviderContext for to implement dark mode
    Created ThemeProviderContext and modularized context with ThemeContext.ts and useTheme.ts to satisfy fast refresh requirements.

---
+++ client/src/contexts/ThemeProviderContext.tsx
@@ -0,0 +1,37 @@
+import { useEffect, ReactNode } from 'react';
+import { ConfigProvider } from 'antd';
+import { lightTheme, darkTheme } from '../styles/themeConfig';
+
+import useLocalStorageState from '../hooks/useLocalStorageState';
+import { ThemeContext } from './ThemeContext';
+
+export function ThemeProviderContext({ children }: { children: ReactNode }) {
+  const [theme, setTheme] = useLocalStorageState('theme');
+
+  // Update root html tag
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+  const themeTokens = theme === 'light' ? lightTheme : darkTheme;
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ConfigProvider
+        theme={{
+          token: { ...themeTokens },
+          components: {
+            Tabs: {
+              itemColor: '#ffffff',
+              inkBarColor: '#1677ff',
+            },
+          },
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </ThemeContext.Provider>
+  );
+}
