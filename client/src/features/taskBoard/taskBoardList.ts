e4135e4fb00d7f0b7e8ba4c6a0ec6d9fb5c636aa
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-03 11:04:12 -0800

    fix(frontend): remove taskBoardList.ts (May implement later)

--- client/src/features/taskBoard/taskBoardList.ts
+++
@@ -1,6 +0,0 @@
-// Columns are based off of the Status in the Schema BACKLOG. IN_PROGRESS, DONE
-import { useGetTickets } from '../tickets/useGetTickets';
-
-export function taskBoardList() {
-  return null;
-}
