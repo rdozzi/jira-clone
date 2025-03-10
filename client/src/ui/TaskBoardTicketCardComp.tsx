0f0f8cca0cbfb6b34afacffde78aeaa207a9fc7c
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-09 18:09:47 -0700

    fix(frontend): add forwardRef and ref as prop within memo; changed innerRef to ref to maintain convention.

--- client/src/ui/TaskBoardTicketCardComp.tsx
+++ client/src/ui/TaskBoardTicketCardComp.tsx
@@ -1,48 +1,58 @@
-import { memo } from 'react';
+import { memo, forwardRef } from 'react';
 
 import { Card } from 'antd';
 import TicketListItemButton from './TicketListItemButton';
 
-const TaskBoardTicketCardComp = memo(function TaskBoardTicketCardComp({
-  draggableProps,
-  dragHandleProps,
-  innerRef,
-  ticket, //Individual ticket information
-  children,
-}) {
-  return (
-    <Card
-      {...draggableProps}
-      {...dragHandleProps}
-      ref={innerRef}
-      bordered={false}
-      size='small'
-      title={
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            position: 'relative',
-            width: '100%',
-          }}
-        >
-          <span style={{ fontWeight: 'bold' }}>{ticket.title}</span>
-          <span
+const TaskBoardTicketCardComp = memo(
+  forwardRef(function TaskBoardTicketCardComp(
+    {
+      draggableProps,
+      dragHandleProps,
+      ticket, //Individual ticket information
+      children,
+    },
+    ref
+  ) {
+    return (
+      <Card
+        {...draggableProps}
+        {...dragHandleProps}
+        ref={ref}
+        bordered={false}
+        size='small'
+        title={
+          <div
             style={{
-              position: 'absolute',
-              right: 0,
-              cursor: 'pointer',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'relative',
+              width: '100%',
             }}
           >
-            <TicketListItemButton record={ticket} />
-          </span>
-        </div>
-      }
-    >
-      {children}
-    </Card>
-  );
-});
+            <span style={{ fontWeight: 'bold' }}>{ticket.title}</span>
+            <span
+              style={{
+                position: 'absolute',
+                right: 0,
+                cursor: 'pointer',
+              }}
+            >
+              <TicketListItemButton record={ticket} />
+            </span>
+          </div>
+        }
+      >
+        {children}
+      </Card>
+    );
+  }),
+
+  arePropsEqual
+);
+
+function arePropsEqual(prevProps, nextProps) {
+  return prevProps.ticket === nextProps.ticket;
+}
 
 export default TaskBoardTicketCardComp;
