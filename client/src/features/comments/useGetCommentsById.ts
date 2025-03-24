387c8ecdae768afb7f482cd8c0bf9066bb30364a
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-24 15:21:26 -0700

    feat(frontend): create useGetCommentsById query command to process getCommentsById

---
+++ client/src/features/comments/useGetCommentsById.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCommentsById as apiGetCommentsById } from '../../services/apiComments';
+
+export function useGetCommentsById(commentId: number) {
+  const {
+    isLoading: isFetching,
+    data: comments,
+    error,
+  } = useQuery({
+    queryKey: ['ticket', commentId],
+    queryFn: () => apiGetCommentsById(commentId),
+    enabled: !!commentId, // Only run the query if commentId is truthy
+  });
+
+  return { isFetching, comments, error };
+}
