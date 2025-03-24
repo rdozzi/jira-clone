00307c7d1cb603c9dcc7a6489a159e185e056d40
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-24 10:39:50 -0700

    fix(frontend): update setQueryData logic to fix undefined re-rendering issue with TicketList

--- client/src/features/tickets/useUpdateTicket.ts
+++ client/src/features/tickets/useUpdateTicket.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateTicket as apiUpdateTicket } from '../../services/apiTickets';
+import { Tickets } from '../../types/Tickets';
 
 export function useUpdateTicket() {
   const queryClient = useQueryClient();
@@ -9,13 +10,14 @@ export function useUpdateTicket() {
       return apiUpdateTicket(ticketId, values);
     },
     onSuccess: (updatedTicket) => {
-      queryClient.setQueryData(['tickets'], (oldTickets: any[] | undefined) => {
-        if (!oldTickets) return oldTickets;
-        return oldTickets.map((ticket) =>
-          ticket.id === updatedTicket.id ? updateTicket : ticket
-        );
+      queryClient.setQueryData(['tickets'], (oldTickets: Tickets[] = []) => {
+        return oldTickets.map((t) => {
+          if (t.id === updatedTicket.id) {
+            return { ...t, ...updatedTicket };
+          }
+          return t;
+        });
       });
-      queryClient.invalidateQueries({ queryKey: ['tickets'] });
     },
     onError: (error) => {
       console.error('Mutation Failed:', error);
