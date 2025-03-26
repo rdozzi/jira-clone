eed563b5df1052dc7f28982b76ab8b6fe3be8b2c
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-25 18:05:32 -0700

    feat(frontend): implement createComment function in apiComments to call comments endpoint

--- client/src/services/apiComments.ts
+++ client/src/services/apiComments.ts
@@ -11,3 +11,23 @@ export async function getCommentsById(ticketId: number) {
     console.error(err);
   }
 }
+
+// Create a comment by ticket Id - Requires ticketId, authorId, content
+export async function createComment(commentObject: object) {
+  try {
+    const res = await fetch('http://localhost:3000/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(commentObject),
+    });
+    if (!res.ok) {
+      throw new Error('Failed to create ticket');
+    }
+    const data = await res.json();
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
