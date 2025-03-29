6846e408ec8aa3dc9e5a27d751e23a74ef0565e9
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-28 21:15:28 -0700

    refactor(frontend): update hook argument to recordId (to semantically correct it from commentId); updated queryKey to ['comments-by-ticket',recordId] for all hooks

--- client/src/features/comments/useGetCommentsById.ts
+++ client/src/features/comments/useGetCommentsById.ts
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCommentsById as apiGetCommentsById } from '../../services/apiComments';
 
-export function useGetCommentsById(commentId: number) {
+export function useGetCommentsById(recordId: number) {
   const {
     isLoading: isFetching,
     data: comments,
     error,
   } = useQuery({
-    queryKey: ['comment', commentId],
-    queryFn: () => apiGetCommentsById(commentId),
-    enabled: !!commentId, // Only run the query if commentId is truthy
+    queryKey: ['comments-by-ticket', recordId],
+    queryFn: () => apiGetCommentsById(recordId),
+    enabled: !!recordId, // Only run the query if commentId is truthy
   });
 
   return { isFetching, comments, error };
