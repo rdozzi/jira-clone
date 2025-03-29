fbe6a9b87e09a2e6a335e39ae93fbfab6dd33409
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-28 21:15:28 -0700

    refactor(frontend): update hook argument to recordId (to semantically correct it from commentId); updated queryKey to ['comments-by-ticket',recordId] for all hooks

--- client/src/features/comments/useCreateComment.ts
+++ client/src/features/comments/useCreateComment.ts
@@ -1,12 +1,14 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createComment as apiCreateComment } from '../../services/apiComments';
 
-export function useCreateComment(commentId: number) {
+export function useCreateComment(recordId: number) {
   const queryClient = useQueryClient();
   const { mutate: createNewComment, status } = useMutation({
     mutationFn: apiCreateComment,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['comment', commentId] });
+      queryClient.invalidateQueries({
+        queryKey: ['comments-by-ticket', recordId],
+      });
     },
   });
 
