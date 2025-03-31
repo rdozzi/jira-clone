fe693dbc47f5eb01fe1604700560474ceb0280e3
Raphael Dozzi <rdozzi84@gmail.com>
2025-03-30 17:25:09 -0700

    refactor(frontend): create CreateCommentForm to handle the comment form state

---
+++ client/src/ui/CreateCommentForm.tsx
@@ -0,0 +1,39 @@
+import { Form, Input, Button } from 'antd';
+
+import { FormInstance } from 'antd';
+
+interface CreateCommentFormProps {
+  handleCreateComment: (_values: { content: string }) => Promise<void>;
+  isCreating: boolean;
+  form: FormInstance;
+}
+
+function CreateCommentForm({
+  handleCreateComment,
+  isCreating,
+  form,
+}: CreateCommentFormProps) {
+  return (
+    <div>
+      <Form onFinish={handleCreateComment} form={form}>
+        <Form.Item label='Comment' name='content' style={{ display: 'grid' }}>
+          <Input.TextArea
+            placeholder='Enter your comment here'
+            autoSize={{ minRows: 3 }}
+            allowClear={true}
+            showCount
+            minLength={1}
+            maxLength={200}
+          />
+        </Form.Item>
+        <Form.Item label={null}>
+          <Button variant='outlined' htmlType='submit' disabled={isCreating}>
+            Add Comment
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+}
+
+export default CreateCommentForm;
