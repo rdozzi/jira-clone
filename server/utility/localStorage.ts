a7e683e281f87d6ca5ae69c0790e865e2ea147ed
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-09 17:45:09 -0700

    docs(backend): implement code for localStorage; change main function to saveToLocal with FileMetaData typing

--- server/utility/localStorage.ts
+++ server/utility/localStorage.ts
@@ -1,4 +1,23 @@
 import path from 'path';
 import { promises as fs } from 'fs';
+import { FileMetadata } from '../types/file';
 
-export function localStorage(files) {}
+export async function saveToLocal(file): Promise<FileMetadata> {
+  const storagePath = path.join(__dirname, '../uploads');
+  const filePath = path.join(storagePath, file.originalname);
+
+  try {
+    await fs.mkdir(storagePath, { recursive: true });
+    await fs.writeFile(filePath, file.buffer);
+    return {
+      filename: file.originalname,
+      mimetype: file.mimetype,
+      size: file.size,
+      storageLocation: 'local',
+      savedPath: filePath,
+    };
+  } catch (error) {
+    console.error('Error saving file to local storage:', error);
+    throw error;
+  }
+}
