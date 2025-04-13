fb331f6587ba5013abe62eff256e7c36e0cdc9f3
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-13 13:44:58 -0700

    refactor(backend): include payload filter logic within findMany to use /attachment route to get DB entries

--- server/src/router/attachmentRoutes.ts
+++ server/src/router/attachmentRoutes.ts
@@ -1,15 +1,25 @@
 import { Router, Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, AttachmentEntityType } from '@prisma/client';
 
 const router = Router();
 const prisma = new PrismaClient();
 
-// Get all attachments
+// Get all attachments if Entity/Id is not provided
 router.get(
   '/attachments',
   async (req: Request, res: Response): Promise<void> => {
+    const { entityType, entityId } = req.body;
     try {
-      const attachments = await prisma.attachment.findMany();
+      const whereClause =
+        entityType && entityId
+          ? {
+              entityType: entityType as AttachmentEntityType,
+              entityId: Number(entityId),
+            }
+          : undefined;
+      const attachments = await prisma.attachment.findMany({
+        where: whereClause,
+      });
       res.status(200).json(attachments);
     } catch (error) {
       console.error('Error fetching attachments: ', error);
@@ -17,12 +27,12 @@ router.get(
     }
   }
 );
-// Get attachment by Entity/Id
-// Download attachment
-// Download all attachments by Entity/Id
+
 // Create attachment
 // Create several attachments
 // Delete attachment
 // Delete all attachments
+// Download attachment
+// Download all attachments by Entity/Id
 
 export default router;
