939cc575def214ba4cae1298f25ceb4bd673d7fd
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-13 12:09:07 -0700

    feat(backend): implement get all attachments route

--- server/src/router/attachmentRoutes.ts
+++ server/src/router/attachmentRoutes.ts
@@ -4,6 +4,25 @@ import { PrismaClient } from '@prisma/client';
 const router = Router();
 const prisma = new PrismaClient();
 
-export default function attachmentRoutes() {
-  return null;
-}
+// Get all attachments
+router.get(
+  '/attachments',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const attachments = await prisma.attachment.findMany();
+      res.status(200).json(attachments);
+    } catch (error) {
+      console.error('Error fetching attachments: ', error);
+      res.status(500).json({ error: 'Failed to fetch attachments' });
+    }
+  }
+);
+// Get attachment by Entity/Id
+// Download attachment
+// Download all attachments by Entity/Id
+// Create attachment
+// Create several attachments
+// Delete attachment
+// Delete all attachments
+
+export default router;
