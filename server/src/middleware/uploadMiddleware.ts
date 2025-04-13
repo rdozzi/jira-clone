1bbad6620d86d12cd3220f7d8aa3eed7e9ce08d3
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-13 11:06:35 -0700

    feat(backend): implement uploadMiddleware file to handle hard disk and cloud logic

---
+++ server/src/middleware/uploadMiddleware.ts
@@ -0,0 +1,40 @@
+import multer from 'multer';
+import { Request } from 'express';
+import path from 'path';
+import fs from 'fs';
+import { getStorageType } from '../config/storage';
+
+const storageType = getStorageType();
+
+let upload: multer.Multer;
+if (storageType === 'LOCAL') {
+  const localUploadPath = path.join(__dirname, '..', '..', 'uploads');
+
+  if (!fs.existsSync(localUploadPath)) {
+    fs.mkdirSync(localUploadPath, { recursive: true });
+  }
+
+  const storage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb) => {
+      const destination = (req.query.path as string) || localUploadPath;
+      if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination, { recursive: true });
+      }
+      cb(null, destination);
+    },
+    filename: (req: Request, file: Express.Multer.File, cb) => {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null, fileName);
+    },
+  });
+
+  upload = multer({
+    storage,
+  });
+} else if (storageType === 'CLOUD') {
+  upload = multer({ storage: multer.memoryStorage() });
+} else {
+  throw new Error(`Invalid storage type: ${storageType}`);
+}
+
+export const uploadMiddleware = upload.single('file');
