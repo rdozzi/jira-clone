b1507edf99fdd807fa395d97cbfe2b55ad730858
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-13 15:40:18 -0700

    feat(backend): add prisma client as argument from routes and began transfer of prisma calls to controller

--- server/src/controllers/uploadController.ts
+++ server/src/controllers/uploadController.ts
@@ -1,12 +1,37 @@
 import { Request, Response } from 'express';
 import { handleFileUpload } from '../services/uploadService';
+import { PrismaClient } from '@prisma/client';
 
-export async function handleUpload(req: Request, res: Response) {
+export async function handleUpload(
+  req: Request,
+  res: Response,
+  prisma: PrismaClient
+) {
   try {
+    if (!req.file) {
+      return res.status(400).json({
+        message: 'No file uploaded',
+      });
+    }
+    const { entityType, entityId } = req.body;
+    const uploadedBy = (req as any).user?.id || 1; // Will be replaced with the logged-in user ID eventually
     const metadata = await handleFileUpload(req.file as Express.Multer.File);
-    res.status(200).json({
+
+    const attachment = await prisma.attachment.create({
+      data: {
+        entityType,
+        entityId: Number(entityId),
+        uploadedBy,
+        fileName: metadata.filename,
+        fileType: metadata.mimetype,
+        fileSize: metadata.size,
+        filePath: metadata.savedPath,
+        fileUrl: metadata.cloudUrl,
+      },
+    });
+    res.status(201).json({
       message: 'File uploaded successfully',
-      metadata,
+      attachment,
     });
   } catch (error) {
     res.status(500).json({
