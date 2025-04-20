aff3411c80f73959f6516d89f320eab959b84ba6
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-20 14:48:50 -0700

    refactor(backend): update getSignedUrl to generateSignedCloudUrl for clarity

--- server/src/utilities/generateSignedCloudUrl.ts
+++ server/src/utilities/generateSignedCloudUrl.ts
@@ -1,4 +1,6 @@
-export async function getSignedUrl(attachment: { fileUrl: string | null }) {
+export async function generateSignedCloudUrl(attachment: {
+  fileUrl: string | null;
+}) {
   // Use the cloud provider's SDK to generate a signed URL for real implementaiton
   if (!attachment.fileUrl) {
     throw new Error('File URL is null or undefined');
