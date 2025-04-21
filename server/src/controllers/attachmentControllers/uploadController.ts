c6e5f21b0724ed13a79ab6b81de340e0d9d41a11
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-21 13:16:35 -0700

    refactor(backend): move all attachment related controllers to attachmentControllers

--- server/src/controllers/attachmentControllers/uploadController.ts
+++ server/src/controllers/attachmentControllers/uploadController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { handleFileUpload } from '../services/uploadService';
+import { handleFileUpload } from '../../services/uploadService';
 import { PrismaClient } from '@prisma/client';
 
 export async function handleSingleUpload(
