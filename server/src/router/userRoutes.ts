fff0612e907025f61db0ee73becf2e5c6cc7b4ce
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-21 14:52:56 -0700

    feat(backend): create userController file and add routing logic; update userRoutes with logic references

--- server/src/router/userRoutes.ts
+++ server/src/router/userRoutes.ts
@@ -1,69 +1,33 @@
 import { Router, Request, Response } from 'express';
 import prisma from '../lib/prisma';
-import { hashPassword } from '../password';
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  deleteUser,
+} from '../controllers/userController';
 
 const router = Router();
 
 // Get all users
 router.get('/users', async (req: Request, res: Response): Promise<void> => {
-  try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
-  } catch (error) {
-    console.error('Error fetching users: ', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
+  await getAllUsers(req, res, prisma);
 });
 
 // Get user by Id
-
 router.get('/users/:id', async (req: Request, res: Response): Promise<void> => {
-  const { id } = req.params;
-  try {
-    const user = await prisma.user.findUnique({ where: { id: Number(id) } });
-    res.status(200).json(user);
-  } catch (error) {
-    console.error('Error fetching users: ', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
+  await getUserById(req, res, prisma);
 });
 
 // Create user
-
 router.post('/', async (req: Request, res: Response): Promise<void> => {
-  try {
-    // const userDetails = await req.body;
-    const { email, name, passwordHash, role } = req.body;
-    const hashedPassword = await hashPassword(passwordHash);
-    const user = await prisma.user.create({
-      data: {
-        email: email,
-        first_name: name,
-        last_name: name,
-        passwordHash: hashedPassword,
-        role: role,
-      },
-    });
-    res.status(200).json(user);
-  } catch (error) {
-    console.error('Error creating user: ', error);
-    res.status(500).json({ error: 'Failed to create user' });
-  }
+  await createUser(req, res, prisma);
 });
 
 router.delete(
   '/users/:id',
   async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { id } = req.params;
-      const deleteData = await prisma.user.delete({
-        where: { id: Number(id) },
-      });
-      res.status(200).json(deleteData);
-    } catch (error) {
-      console.error('Error fetching users: ', error);
-      res.status(500).json({ error: 'Failed to fetch users' });
-    }
+    await deleteUser(req, res, prisma);
   }
 );
 
