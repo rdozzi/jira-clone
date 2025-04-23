e7716cd16fd6adca2d24f09d681dd716babcc4c2
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-23 15:39:28 -0700

    fix(backend): fix type error
    Added null condition to userId; added the boardId field with number and null

--- server/src/types/logEventPayload.ts
+++ server/src/types/logEventPayload.ts
@@ -4,13 +4,13 @@ type JsonPrimitive = string | number | boolean;
 export type Json = JsonPrimitive | { [key: string]: Json } | Json[];
 
 export type LogEventPayload = {
-  userId?: number;
+  userId?: number | null;
   actorType?: ActorTypeActivity;
   action: string;
   targetId?: number | null;
   targetType: string;
   metadata: Json;
   ticketId?: number | null;
-  projectId?: number | null;
   boardId?: number | null;
+  projectId?: number | null;
 };
