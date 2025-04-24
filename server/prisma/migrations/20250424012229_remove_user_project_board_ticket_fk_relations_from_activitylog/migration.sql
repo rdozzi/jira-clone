7f47e415b70b21f6e8616c93659ba4e42c3d6581
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-23 18:36:03 -0700

    fix(database/prisma): remove foreign key relations in ActivityLog; apply migration

---
+++ server/prisma/migrations/20250424012229_remove_user_project_board_ticket_fk_relations_from_activitylog/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "ActivityLog" DROP CONSTRAINT "ActivityLog_boardId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ActivityLog" DROP CONSTRAINT "ActivityLog_projectId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ActivityLog" DROP CONSTRAINT "ActivityLog_ticketId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ActivityLog" DROP CONSTRAINT "ActivityLog_userId_fkey";
