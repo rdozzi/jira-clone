96c48bc305924b0451c29bac4316caef9db7eed5
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-28 11:47:57 -0700

    feat(backend): implement res.locals.logEvent into createUser for ActivityLog processing

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { hashPassword } from '../password';
+import { buildLogEvent } from '../services/buildLogEvent';
 
 // Get all users
 export async function getAllUsers(
@@ -37,22 +38,38 @@ export async function getUserById(
 export async function createUser(
   req: Request,
   res: Response,
+  next: NextFunction,
   prisma: PrismaClient
 ) {
   try {
-    // const userDetails = await req.body;
-    const { email, name, passwordHash, role } = req.body;
+    const { email, first_name, last_name, passwordHash, role } = req.body;
     const hashedPassword = await hashPassword(passwordHash);
+
     const user = await prisma.user.create({
       data: {
         email: email,
-        first_name: name,
-        last_name: name,
+        first_name: first_name,
+        last_name: last_name,
         passwordHash: hashedPassword,
         role: role,
       },
     });
-    res.status(200).json(user);
+
+    res.locals.logEvent = buildLogEvent({
+      userId: user.id,
+      actorType: 'USER',
+      action: 'CREATE_USER',
+      targetId: user.id,
+      targetType: 'USER',
+      metadata: {
+        role: `${user.role}`,
+        name: `${user.first_name}_${user.last_name}`,
+        email: `${user.email}`,
+      },
+    });
+
+    res.status(201).json(user);
+    next();
   } catch (error) {
     console.error('Error creating user: ', error);
     res.status(500).json({ error: 'Failed to create user' });
@@ -62,6 +79,7 @@ export async function createUser(
 export async function deleteUser(
   req: Request,
   res: Response,
+  next: NextFunction,
   prisma: PrismaClient
 ) {
   try {
@@ -70,6 +88,7 @@ export async function deleteUser(
       where: { id: Number(id) },
     });
     res.status(200).json(deleteData);
+    next();
   } catch (error) {
     console.error('Error fetching users: ', error);
     res.status(500).json({ error: 'Failed to fetch users' });
