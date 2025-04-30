7a6e05a003357040cab23d73fb65c84f855898ab
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-29 19:32:49 -0700

    feat(backend): add res.locals references to delete single and many attachment controller functions respectively

--- server/src/controllers/attachmentControllers/deleteManyAttachmentsController.ts
+++ server/src/controllers/attachmentControllers/deleteManyAttachmentsController.ts
@@ -1,11 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs/promises';
 import { PrismaClient } from '@prisma/client';
+import { buildLogEvent } from '../../services/buildLogEvent';
 
 export async function deleteManyAttachments(
   req: Request,
   res: Response,
+  next: NextFunction,
   prisma: PrismaClient
 ): Promise<void> {
   const { ids } = req.body;
@@ -24,6 +26,10 @@ export async function deleteManyAttachments(
         },
       },
     });
+
+    const deletedAttachments: typeof attachments = JSON.parse(
+      JSON.stringify(attachments)
+    );
 
     for (const attachment of attachments) {
       try {
@@ -48,7 +54,30 @@ export async function deleteManyAttachments(
         },
       },
     });
+
+    res.locals.logEvents = deletedAttachments.map((attachment) => {
+      return buildLogEvent({
+        userId: attachment.uploadedBy,
+        actorType: 'USER',
+        action: 'DELETE_ATTACHMENT',
+        targetId: attachment.id,
+        targetType: 'ATTACHMENT',
+        metadata: {
+          fileName: attachment.fileName,
+          fileType: attachment.entityType,
+          fileSize: attachment.fileSize,
+          filePath: attachment.filePath,
+          fileUrl: attachment.fileUrl,
+          storageType: attachment.storageType,
+        },
+        ticketId: attachment.entityId,
+        boardId: null,
+        projectId: null,
+      });
+    });
+
     res.status(200).json({ message: 'Attachments deleted successfully' });
+    next();
   } catch (error) {
     console.error('Error deleting attachments: ', error);
     res.status(500).json({ error: 'Failed to delete attachments' });
