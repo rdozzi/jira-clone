e016c622fe329a05671a6f58e12d9e0fb5da237b
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-29 21:26:09 -0700

    feat(backend): implement activity log features in single and multiple download controller

--- server/src/controllers/attachmentControllers/downloadController.ts
+++ server/src/controllers/attachmentControllers/downloadController.ts
@@ -1,14 +1,17 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import { generateSignedCloudUrl } from '../../utilities/generateSignedCloudUrl';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Attachment } from '@prisma/client';
+import { buildLogEvent } from '../../services/buildLogEvent';
 
 export async function downloadSingleAttachment(
   req: Request,
   res: Response,
+  next: NextFunction,
   prisma: PrismaClient
 ) {
   const attachmentId = Number(req.params.id);
+  console.log(attachmentId);
   try {
     const attachment = await prisma.attachment.findUnique({
       where: { id: attachmentId },
@@ -20,14 +23,16 @@ export async function downloadSingleAttachment(
 
     if (attachment.storageType === 'LOCAL') {
       const filePath = path.resolve(attachment.filePath || '');
+      res.locals.logEvent = generatePayload(attachment);
       return res.download(filePath, attachment.fileName);
+      next();
     } else if (attachment.storageType === 'CLOUD') {
       const signedUrl = await generateSignedCloudUrl(attachment);
-      // Indicate a successful response with a 201 status code
+      res.locals.logEvent = generatePayload(attachment);
       res.status(201).json({
         message: `Signed URL generated successfully: ${signedUrl}`,
       });
-      // return res.redirect(signedUrl);
+      next();
     } else {
       res.status(400).json({ error: 'Invalid storage type' });
     }
@@ -35,4 +40,25 @@ export async function downloadSingleAttachment(
     console.error('Error downloading attachment: ', error);
     res.status(500).json({ error: 'Failed to download attachment' });
   }
+}
+
+function generatePayload(attachment: Attachment) {
+  return buildLogEvent({
+    userId: attachment.uploadedBy,
+    actorType: 'USER',
+    action: 'DOWNLOAD_ATTACHMENT',
+    targetId: attachment.id,
+    targetType: 'ATTACHMENT',
+    metadata: {
+      fileName: attachment.fileName,
+      fileType: attachment.entityType,
+      fileSize: attachment.fileSize,
+      filePath: attachment.filePath,
+      fileUrl: attachment.fileUrl,
+      storageType: attachment.storageType,
+    },
+    ticketId: attachment.entityId,
+    boardId: null,
+    projectId: null,
+  });
 }
