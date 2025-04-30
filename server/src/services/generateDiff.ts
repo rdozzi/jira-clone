35e294de21f705086b63c287a5bed28c4d033cad
Raphael Dozzi <rdozzi84@gmail.com>
2025-04-29 18:33:34 -0700

    fix(backend): implement if statement to omit createdAt and updatedAt values from metadata; implement logic to accurately check dates for differences

--- server/src/services/generateDiff.ts
+++ server/src/services/generateDiff.ts
@@ -11,10 +11,21 @@ export function generateDiff<
   const diff: DiffResult = {};
 
   for (const key of Object.keys(after)) {
+    if (key === 'createdAt' || key === 'updatedAt') {
+      continue;
+    }
+
     const beforeValue = before[key];
     const afterValue = after[key];
 
-    if (beforeValue !== afterValue) {
+    if (beforeValue instanceof Date && afterValue instanceof Date) {
+      if (beforeValue.getTime() !== afterValue.getTime()) {
+        diff[key] = {
+          before: beforeValue,
+          after: afterValue,
+        };
+      }
+    } else if (beforeValue !== afterValue) {
       diff[key] = {
         before: beforeValue,
         after: afterValue,
