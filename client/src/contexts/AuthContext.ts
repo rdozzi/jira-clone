475b04acc212d950a503f8d26b3e597cb50b4579
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-04 10:09:46 -0700

    feat(frontend): create AuthContext and AutProviderContext for app-wide authorization scheme

---
+++ client/src/contexts/AuthContext.ts
@@ -0,0 +1,22 @@
+import { createContext, useContext } from 'react';
+
+type UserRole = 'GUEST' | 'USER' | 'ADMIN' | null;
+type AuthContextType = {
+  userRole: UserRole;
+  loginAs: (_role: UserRole) => void;
+  logout: () => void;
+};
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
+// This is a custom hook that provides the authentication context to components
+// that need to access the authentication state and methods.
