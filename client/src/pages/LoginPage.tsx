1492b83f429d6a98fda42cbe47ddb8939c603b30
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-05 12:33:11 -0700

    feat(frontend): add reset button to Login form

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -5,7 +5,9 @@ import { useAuth } from '../contexts/AuthContext';
 import { UserRole } from '../types/UserRole';
 
 import type { FormProps } from 'antd';
-import { Layout, Card, Button, Checkbox, Form, Input } from 'antd';
+import { Layout, Card, Button, Checkbox, Form, Input, Space } from 'antd';
+
+const { Content } = Layout;
 
 type FieldType = {
   email?: string;
@@ -13,9 +15,19 @@ type FieldType = {
   remember?: string;
 };
 
-const { Content } = Layout;
+interface LoginFormValues {
+  email: string;
+  password: string;
+  remember: boolean;
+}
+
+interface OnFinishFailedErrorInfo {
+  errorFields: Array<{ name: (string | number)[]; errors: string[] }>;
+  outOfDate: boolean;
+}
 
 function LoginPage() {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
   const { login } = useAuth();
 
@@ -41,19 +53,8 @@ function LoginPage() {
     navigate(redirectPath, { replace: true });
   }
 
-  interface LoginFormValues {
-    email: string;
-    password: string;
-    remember: boolean;
-  }
-
   function onFinish(values: LoginFormValues): void {
     console.log('Received values of form: ', values);
-  }
-
-  interface OnFinishFailedErrorInfo {
-    errorFields: Array<{ name: (string | number)[]; errors: string[] }>;
-    outOfDate: boolean;
   }
 
   function onFinishFailed(errorInfo: OnFinishFailedErrorInfo): void {
@@ -79,12 +80,19 @@ function LoginPage() {
             onFinish={onFinish}
             onFinishFailed={onFinishFailed}
             autoComplete='off'
+            form={form}
           >
             {/* Email Input */}
             <Form.Item<FieldType>
               label='Email'
               name='email'
-              rules={[{ required: true, message: 'Please enter your email!' }]}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter a valid email!',
+                  type: 'email',
+                },
+              ]}
             >
               <Input />
             </Form.Item>
@@ -94,7 +102,10 @@ function LoginPage() {
               label='Password'
               name='password'
               rules={[
-                { required: true, message: 'Please enter your password!' },
+                {
+                  required: true,
+                  message: 'Please enter your password!',
+                },
               ]}
             >
               <Input.Password />
@@ -109,12 +120,26 @@ function LoginPage() {
               <Checkbox>Remember me</Checkbox>
             </Form.Item>
 
-            {/* Submit Button */}
-            <Form.Item label={null}>
-              <Button type='primary' htmlType='submit'>
-                Submit
-              </Button>
-            </Form.Item>
+            <Space>
+              {/* Submit Button */}
+              <Form.Item label={null}>
+                <Button type='primary' htmlType='submit'>
+                  Submit
+                </Button>
+              </Form.Item>
+
+              {/* Reset */}
+              <Form.Item label={null}>
+                <Button
+                  htmlType='button'
+                  onClick={() => {
+                    form.resetFields();
+                  }}
+                >
+                  Reset
+                </Button>
+              </Form.Item>
+            </Space>
           </Form>
         </Card>
       </Content>
