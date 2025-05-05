ff1c602546fbc277d310a8bbf8fedfa61fd04889
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-05 14:43:43 -0700

    feat(backend): update user routes and controller functions to accept req.query
    Updated getUserById to getUser to take both either id or email to fetch users. Updated the route accordingly; updated userRuotes for clarity and route file functionality

--- server/src/router/userRoutes.ts
+++ server/src/router/userRoutes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express';
 import prisma from '../lib/prisma';
 import {
   getAllUsers,
-  getUserById,
+  getUser,
   createUser,
   deleteUser,
   updateUser,
@@ -11,13 +11,13 @@ import {
 const router = Router();
 
 // Get all users
-router.get('/users', async (req: Request, res: Response): Promise<void> => {
+router.get('/users/all', async (req: Request, res: Response): Promise<void> => {
   await getAllUsers(req, res, prisma);
 });
 
-// Get user by Id
-router.get('/users/:id', async (req: Request, res: Response): Promise<void> => {
-  await getUserById(req, res, prisma);
+// Get user
+router.get('/users', async (req: Request, res: Response): Promise<void> => {
+  await getUser(req, res, prisma);
 });
 
 // Create user
