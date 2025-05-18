1423fccd55159779d19e192d413a688fb0311ef4
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-18 14:50:05 -0700

    feat(backend): add route and function to get logs by ticket id

--- server/src/controllers/activityController.ts
+++ server/src/controllers/activityController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
+// Get all routes
 export async function getAllRoutes(
   req: Request,
   res: Response,
@@ -12,5 +13,34 @@ export async function getAllRoutes(
   } catch (error) {
     console.error('Error fetching routes: ', error);
     res.status(500).json({ error: 'Failed to fetch routes' });
+  }
+}
+
+// Get logs by ticketId
+export async function getRoutebyTicketId(
+  req: Request,
+  res: Response,
+  prisma: PrismaClient
+) {
+  try {
+    const { ticketId } = req.params;
+    const ticketIdNumber = parseInt(ticketId, 10);
+
+    if (!ticketId) {
+      return res.status(400).json({ error: 'Ticket ID is required' });
+    }
+
+    const logs = await prisma.activityLog.findMany({
+      where: { ticketId: ticketIdNumber },
+    });
+
+    if (!logs) {
+      return res.status(404).json({ error: 'No logs found' });
+    }
+
+    res.status(200).json(logs);
+  } catch (error) {
+    console.error('Error fetching logs by ticket ID: ', error);
+    res.status(500).json({ error: 'Failed to fetch logs' });
   }
 }
--- server/src/router/activityLogRoutes.ts
+++ server/src/router/activityLogRoutes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response } from 'express';
 import prisma from '../lib/prisma';
-import { getAllRoutes } from '../controllers/activityController';
+import {
+  getAllRoutes,
+  getRoutebyTicketId,
+} from '../controllers/activityController';
 
 const router = Router();
 
 // Get all logs
-
 router.get(
   '/activity-logs/all',
   async (req: Request, res: Response): Promise<void> => {
@@ -13,4 +15,10 @@ router.get(
   }
 );
 
+router.get(
+  '/activity-logs/:ticketId',
+  async (req: Request, res: Response): Promise<void> => {
+    await getRoutebyTicketId(req, res, prisma);
+  }
+);
 export default router;
