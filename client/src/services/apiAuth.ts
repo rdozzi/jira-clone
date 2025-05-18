3088cea7c988af14a61c479dd27258c80e790120
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-18 10:23:25 -0700

    feat(frontend): create frontend logout endpoint api
    Created logout function as frontend endpoint call

--- client/src/services/apiAuth.ts
+++ client/src/services/apiAuth.ts
@@ -25,3 +25,21 @@ export async function login({
     console.error(err);
   }
 }
+
+export async function logout(token: string) {
+  try {
+    const res = await fetch('http://localhost:3000/api/auth/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error('Failed to log out');
+    }
+    return res;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
