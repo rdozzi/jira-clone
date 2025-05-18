e084dda701187ccdc35879e92e11fd27192a966b
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-18 10:18:42 -0700

    feat(backend): update authController with activity log and logout controller function
    Added next function and buildlogEvent function to loginUser; Implemented logoutUser with ActivityLog table register

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { NextFunction, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 import { verifyPassword } from '../utilities/password';
-import dotenv from 'dotenv';
+import { buildLogEvent } from '../services/buildLogEvent';
 
 dotenv.config();
 
 export async function loginUser(
   req: Request,
   res: Response,
+  next: NextFunction,
   prisma: PrismaClient
 ) {
   try {
@@ -18,7 +20,7 @@ export async function loginUser(
       where: { email: userData.email },
     });
 
-    if (!user) {
+    if (!user || user.deletedAt) {
       return res.status(401).json({ error: 'No user found' });
     }
 
@@ -39,6 +41,24 @@ export async function loginUser(
       }
     );
 
+    res.locals.logEvent = buildLogEvent({
+      userId: null,
+      actorType: 'USER',
+      action: 'USER_LOGIN',
+      targetId: user.id,
+      targetType: 'AUTHENTICATION',
+      metadata: {
+        id: user.id,
+        name: `${user.firstName}_${user.lastName}`,
+        email: user.email,
+        globalRole: user.globalRole,
+        token: token,
+      },
+      ticketId: null,
+      boardId: null,
+      projectId: null,
+    });
+
     res.status(200).json({
       message: 'Login successful',
       token,
@@ -46,8 +66,45 @@ export async function loginUser(
       userRole: user.globalRole,
       expiresIn: Date.now() + Number(process.env.JWT_EXPIRATION) * 1000,
     });
+    next();
   } catch (error) {
     console.error('Error logging in user: ', error);
     res.status(500).json({ error: 'Failed to log in user' });
+  }
+}
+
+export async function logoutUser(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    const userId = parseInt((decoded as { id: string }).id, 10);
+
+    res.locals.logEvent = buildLogEvent({
+      userId: null,
+      actorType: 'USER',
+      action: 'USER_LOGOUT',
+      targetId: userId,
+      targetType: 'AUTHENTICATION',
+      metadata: {
+        id: userId,
+        token: token,
+      },
+      ticketId: null,
+      boardId: null,
+      projectId: null,
+    });
+
+    next();
+    res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    console.error('Error logging out user: ', error);
+    res.status(500).json({ error: 'Failed to log out user' });
   }
 }
