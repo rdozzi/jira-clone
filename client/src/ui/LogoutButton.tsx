faab2696184627a77dc49aaf10359f54123059bb
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-18 10:35:30 -0700

    feat(frontend): update LogoutButton logic with front/backend logout features
    Distinguished between the frontend context logout (frontendLogout) and backend logout mutation call (logoutMutation); Implemented onSuccess call for frontend context logout call and navigration to public login page.

--- client/src/ui/LogoutButton.tsx
+++ client/src/ui/LogoutButton.tsx
@@ -1,18 +1,28 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
+import { useLogout } from '../features/auth/useLogout';
 
 import { Button } from 'antd';
 import { LogoutOutlined } from '@ant-design/icons';
 
 function LogoutButton() {
-  const { logout } = useAuth();
+  const { logout: frontendLogout } = useAuth();
+  const { mutate: logoutMutation } = useLogout();
   const navigate = useNavigate();
 
   function handleLogout() {
-    // Perform any additional logout logic here if needed
     console.log('Logging out...');
-    logout();
-    navigate('/login', { replace: true });
+
+    logoutMutation(undefined, {
+      onSuccess: () => {
+        console.log('Logout successful');
+        frontendLogout();
+        navigate('/login', { replace: true });
+      },
+      onError: (error) => {
+        console.error('Logout failed:', error);
+      },
+    });
   }
 
   return (
