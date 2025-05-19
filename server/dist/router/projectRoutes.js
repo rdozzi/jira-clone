d82299fa805b4b5668c4ec2fee391931996cf2a3
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-19 12:40:47 -0700

    feat(backend): implement authenticate and authorize middleware into Create Project route

--- server/dist/router/projectRoutes.js
+++ server/dist/router/projectRoutes.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const prisma_1 = __importDefault(require("../lib/prisma"));
 const projectController_1 = require("../controllers/projectController");
+const authorize_1 = require("../middleware/authorize");
+const authenticate_1 = require("../middleware/authenticate");
+const client_1 = require("@prisma/client");
 const router = (0, express_1.Router)();
 // Get all projects
 router.get('/projects', async (req, res) => {
@@ -16,7 +19,7 @@ router.get('/projects/:id', async (req, res) => {
     await (0, projectController_1.getProjectById)(req, res, prisma_1.default);
 });
 // Create project
-router.post('/projects', async (req, res, next) => {
+router.post('/projects', authenticate_1.authenticate, (0, authorize_1.authorize)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
     await (0, projectController_1.createProject)(req, res, next, prisma_1.default);
 });
 // Update project
