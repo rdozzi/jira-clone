fe037e1b8c5330a76c82407bec7b16e343c246ad
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-19 13:38:03 -0700

    feat(backend): add authenticate and auth middleware to all activity log routes

--- server/src/router/activityLogRoutes.ts
+++ server/src/router/activityLogRoutes.ts
@@ -5,12 +5,17 @@ import {
   getLogbyTicketId,
   getLogbyUserId,
 } from '../controllers/activityController';
+import { authenticate } from '../middleware/authenticate';
+import { authorize } from '../middleware/authorize';
+import { GlobalRole } from '@prisma/client';
 
 const router = Router();
 
 // Get all logs
 router.get(
   '/activity-logs/all',
+  authenticate,
+  authorize(GlobalRole.ADMIN),
   async (req: Request, res: Response): Promise<void> => {
     await getAllLogs(req, res, prisma);
   }
@@ -18,6 +23,8 @@ router.get(
 
 router.get(
   '/activity-logs/:ticketId/ticket',
+  authenticate,
+  authorize(GlobalRole.ADMIN),
   async (req: Request, res: Response): Promise<void> => {
     await getLogbyTicketId(req, res, prisma);
   }
@@ -25,6 +32,8 @@ router.get(
 
 router.get(
   '/activity-logs/:userId/user',
+  authenticate,
+  authorize(GlobalRole.ADMIN),
   async (req: Request, res: Response): Promise<void> => {
     await getLogbyUserId(req, res, prisma);
   }
