8d2dd29593f27741a227a83d7af8c5135d2e6bc0
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-20 19:49:51 -0700

    refactor(backend): update index.ts file with global authenticate
    Refactored index.ts into Public and Protected routes with application of authenticate generally to all protected routes

--- server/dist/index.js
+++ server/dist/index.js
@@ -16,6 +16,7 @@ const authRoutes_1 = __importDefault(require("./router/authRoutes"));
 const activityLogRoutes_1 = __importDefault(require("./router/activityLogRoutes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
+const authenticate_1 = require("./middleware/authenticate");
 const logMiddleware_1 = require("./middleware/logMiddleware");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -26,6 +27,11 @@ app.get('/', (req, res) => {
     res.send('Hello! This is your first app with Express.js, Typescript, and Nodemon!');
 });
 app.use((0, logMiddleware_1.globalLogMiddleware)());
+// Public routes
+app.use('/api', authRoutes_1.default);
+// Authentication middleware (for protected routes)
+app.use(authenticate_1.authenticate);
+// Protected routes
 app.use('/api', userRoutes_1.default);
 app.use('/api', bannedEmailRoutes_1.default);
 app.use('/api', projectRoutes_1.default);
@@ -34,7 +40,6 @@ app.use('/api', commentRoutes_1.default);
 app.use('/api', attachmentRoutes_1.default);
 app.use('/api', boardRoutes_1.default);
 app.use('/api', labelRoutes_1.default);
-app.use('/api', authRoutes_1.default);
 app.use('/api', activityLogRoutes_1.default);
 app.listen(process.env.PORT, () => {
     console.log(`Server is Running on port ${process.env.PORT} with Nodemon!`);
