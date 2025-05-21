8accb3fdb7f16afadeabe35f5aa5a1ed6964f585
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-20 20:07:59 -0700

    refactor(backend): update all current reference to authorizeGlobalRole

--- server/dist/router/projectRoutes.js
+++ server/dist/router/projectRoutes.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const prisma_1 = __importDefault(require("../lib/prisma"));
 const projectController_1 = require("../controllers/projectController");
-const authorize_1 = require("../middleware/authorize");
+const authorizeGlobalRole_1 = require("../middleware/authorizeGlobalRole");
 const client_1 = require("@prisma/client");
 const router = (0, express_1.Router)();
 // Get all projects
@@ -18,15 +18,15 @@ router.get('/projects/:id', async (req, res) => {
     await (0, projectController_1.getProjectById)(req, res, prisma_1.default);
 });
 // Create project
-router.post('/projects', (0, authorize_1.authorize)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
+router.post('/projects', (0, authorizeGlobalRole_1.authorizeGlobalRole)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
     await (0, projectController_1.createProject)(req, res, next, prisma_1.default);
 });
 // Update project
-router.patch('/projects/:id', (0, authorize_1.authorize)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
+router.patch('/projects/:id', (0, authorizeGlobalRole_1.authorizeGlobalRole)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
     await (0, projectController_1.updateProject)(req, res, next, prisma_1.default);
 });
 // Delete project
-router.delete('/projects/:id', (0, authorize_1.authorize)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
+router.delete('/projects/:id', (0, authorizeGlobalRole_1.authorizeGlobalRole)(client_1.GlobalRole.ADMIN), async (req, res, next) => {
     await (0, projectController_1.deleteProject)(req, res, next, prisma_1.default);
 });
 exports.default = router;
