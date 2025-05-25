31de694d322e789de3e0ff9d057cc4b093aa083d
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-24 20:10:55 -0700

    feat(backend): add hasRequiredProjectRole to roles.ts
    Created hasRequiredProjectRole to roles.ts for project role check; extended definitions to include projectRoleHierarchy and ProjectAppRole with ProjectRole enum from prisma; updated original roleHierarchy to globalRoleHierarchy.

--- server/src/lib/roles.ts
+++ server/src/lib/roles.ts
@@ -1,15 +1,27 @@
-import { GlobalRole } from '@prisma/client';
+import { GlobalRole, ProjectRole } from '@prisma/client';
 
-export const roleHierarchy = Object.values(GlobalRole);
+export const globalRoleHierarchy = Object.values(GlobalRole);
+export const projectRoleHierarchy = Object.values(ProjectRole);
 
-export type AppRole = (typeof roleHierarchy)[number];
+export type GlobalAppRole = (typeof globalRoleHierarchy)[number];
+export type ProjectAppRole = (typeof projectRoleHierarchy)[number];
 
 export function hasRequiredGlobalRole(
-  userRole: AppRole,
-  requiredRole: AppRole
+  userRole: GlobalAppRole,
+  requiredRole: GlobalAppRole
 ): boolean {
-  const userIndex = roleHierarchy.indexOf(userRole);
-  const requiredIndex = roleHierarchy.indexOf(requiredRole);
+  const userIndex = globalRoleHierarchy.indexOf(userRole);
+  const requiredIndex = globalRoleHierarchy.indexOf(requiredRole);
+
+  return userIndex >= requiredIndex;
+}
+
+export function hasRequiredProjectRole(
+  userRole: ProjectAppRole,
+  requiredRole: ProjectAppRole
+): boolean {
+  const userIndex = projectRoleHierarchy.indexOf(userRole);
+  const requiredIndex = projectRoleHierarchy.indexOf(requiredRole);
 
   return userIndex >= requiredIndex;
 }
