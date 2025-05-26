207cee20b5edbf745a12a343c098a3ab553db760
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-25 19:52:10 -0700

    feat(backend): create checkProjectMembership and checkProjectRole
    Created both functions to act as separate middleware for project-level authorization checks.

---
+++ server/src/middleware/checkProjectMembership.ts
@@ -0,0 +1,43 @@
+import { Response, NextFunction } from 'express';
+import { CustomRequest } from '../types/CustomRequest';
+import prisma from '../lib/prisma';
+
+export async function checkProjectMembership(
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const userId = req.user?.id;
+    const ticketId = req.params.ticketId;
+    const ticketIdNumber = parseInt(ticketId, 10);
+
+    if (!userId) {
+      res.status(403).json({ message: 'No User Id defined' });
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+      where: { id: ticketIdNumber },
+      include: {
+        board: { include: { project: true } },
+      },
+    });
+
+    const projectId = ticket?.board.project.id;
+
+    const member = await prisma.projectMember.findFirst({
+      where: { projectId, userId: userId },
+    });
+
+    if (!member) {
+      res.status(403).json({ message: 'Forbidden: Not a Project Member' });
+      return;
+    }
+
+    res.locals.projectMember = member;
+    next();
+  } catch (error) {
+    console.error('Auth Error:', error);
+    res.status(500).json({ message: 'Server error during auth check' });
+  }
+}
