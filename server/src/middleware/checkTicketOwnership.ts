c376fed8c78daacc3f85b07ca52d64a8e6ce8527
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-27 15:44:29 -0700

    feat(backend): create checkTicketOwnership check

---
+++ server/src/middleware/checkTicketOwnership.ts
@@ -0,0 +1,28 @@
+import { RequestHandler } from 'express';
+import { CustomRequest } from '../types/CustomRequest';
+import { PrismaClient } from '@prisma/client';
+
+export function checkTicketOwnership(prisma: PrismaClient): RequestHandler {
+  return async (req, res, next) => {
+    const customReq = req as unknown as CustomRequest;
+
+    const userId = customReq.user?.id;
+    const userRole = customReq.user?.role;
+    const ticketId = parseInt(customReq.params.id, 10);
+
+    // If the user is a SUPERADMIN, allow access to all comments
+    if (userRole === 'SUPERADMIN') {
+      return next();
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+      where: { id: ticketId },
+    });
+
+    if (!ticket || ticket.assigneeId !== userId) {
+      res.status(403).json({ message: 'Forbidden: Not your ticket' });
+      return;
+    }
+    next();
+  };
+}
