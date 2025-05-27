8abebb896300e55929ef97d62383833570393744
Raphael Dozzi <rdozzi84@gmail.com>
2025-05-27 16:05:39 -0700

    feat(backend): add auth middleware to board routes

--- server/src/router/boardRoutes.ts
+++ server/src/router/boardRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { CustomRequest } from '../types/CustomRequest';
+import { GlobalRole, ProjectRole } from '@prisma/client';
+import { authorizeGlobalRole } from '../middleware/authorizeGlobalRole';
+import { checkProjectMembership } from '../middleware/checkProjectMembership';
+import { checkProjectRole } from '../middleware/checkProjectRole';
 import prisma from '../lib/prisma';
 import {
   getAllBoards,
@@ -13,13 +17,18 @@ import {
 const router = Router();
 
 // Get all Boards
-router.get('/boards', async (req: Request, res: Response): Promise<void> => {
-  await getAllBoards(req, res, prisma);
-});
+router.get(
+  '/boards',
+  authorizeGlobalRole(GlobalRole.ADMIN),
+  async (req: Request, res: Response): Promise<void> => {
+    await getAllBoards(req, res, prisma);
+  }
+);
 
 // Get board by Id
 router.get(
   '/boards/:id',
+  authorizeGlobalRole(GlobalRole.ADMIN),
   async (req: Request, res: Response): Promise<void> => {
     await getBoardById(req, res, prisma);
   }
@@ -27,6 +36,9 @@ router.get(
 // Get boards by Project Id
 router.get(
   '/boards/:projectId/project',
+  (req: Request, res: Response, next: NextFunction) =>
+    checkProjectMembership(req as CustomRequest, res, next),
+  checkProjectRole(ProjectRole.VIEWER),
   async (req: Request, res: Response): Promise<void> => {
     await getBoardsByProjectId(req, res, prisma);
   }
@@ -35,6 +47,9 @@ router.get(
 // Create board
 router.post(
   '/boards',
+  (req: Request, res: Response, next: NextFunction) =>
+    checkProjectMembership(req as CustomRequest, res, next),
+  checkProjectRole(ProjectRole.ADMIN),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await createBoard(req as CustomRequest, res, next, prisma);
   }
@@ -43,6 +58,9 @@ router.post(
 // Update board
 router.patch(
   '/boards/:boardId',
+  (req: Request, res: Response, next: NextFunction) =>
+    checkProjectMembership(req as CustomRequest, res, next),
+  checkProjectRole(ProjectRole.ADMIN),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await updateBoard(req as CustomRequest, res, next, prisma);
   }
@@ -51,6 +69,9 @@ router.patch(
 // Delete board
 router.delete(
   '/boards/:boardId',
+  (req: Request, res: Response, next: NextFunction) =>
+    checkProjectMembership(req as CustomRequest, res, next),
+  checkProjectRole(ProjectRole.ADMIN),
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     await deleteBoard(req as CustomRequest, res, next, prisma);
   }
