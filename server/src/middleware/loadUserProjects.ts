fcb6df398423426633fe438d7e685648251f0824
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-02 14:33:01 -0700

    feat(backend): create loadUserProjects file
    Created loadUserProjects file as middleware to create and load userProjects as part of the global request object

---
+++ server/src/middleware/loadUserProjects.ts
@@ -0,0 +1,39 @@
+import { Response, NextFunction, RequestHandler } from 'express';
+import { CustomRequest } from '../types/CustomRequest';
+import prisma from '../lib/prisma';
+
+async function loadUserProjectsFn(
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    if (!req.user?.globalRole) {
+      console.error('User role missing in user object');
+      res
+        .status(500)
+        .json({ message: 'User role is missing. Contact user support.' });
+    }
+
+    if (req.user?.globalRole === 'SUPERADMIN') {
+      next();
+    }
+
+    const projectMemberships = await prisma.projectMember.findMany({
+      where: { userId: req.user?.id },
+      select: { projectId: true, projectRole: true },
+    });
+
+    req.userProjects = projectMemberships.map((m) => {
+      return { projectId: m.projectId, projectRole: m.projectRole };
+    });
+    next();
+  } catch (error) {
+    console.error('Error loading user projects: ', error);
+    res
+      .status(500)
+      .json({ message: 'Server error loading project membership' });
+  }
+}
+
+export const loadUserProjects = loadUserProjectsFn as unknown as RequestHandler;
