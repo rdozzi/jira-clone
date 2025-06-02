34461c5a832d50493d0f959a5f67b690bdd9d421
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-02 16:46:32 -0700

    refactor(backend): remove ticket references; update references to globalRole
    Updated file to remove all ticket-specific references; Implemented userProjects from req.userProjects object created upon authentication; Added return statement to catch error black to ensure function termination

--- server/src/middleware/checkProjectMembership.ts
+++ server/src/middleware/checkProjectMembership.ts
@@ -1,6 +1,5 @@
 import { Response, NextFunction } from 'express';
 import { CustomRequest } from '../types/CustomRequest';
-import prisma from '../lib/prisma';
 
 export async function checkProjectMembership(
   req: CustomRequest,
@@ -9,9 +8,11 @@ export async function checkProjectMembership(
 ): Promise<void> {
   try {
     const userId = req.user?.id;
-    const userRole = req.user?.role;
-    const ticketId = req.params.ticketId;
-    const ticketIdNumber = parseInt(ticketId, 10);
+    const userGlobalRole = req.user?.globalRole;
+    const userProjects = req.userProjects; // User project associations
+
+    const { projectId } = req.params; // For ProjectMember specific selections
+    const requestedProjectId = parseInt(projectId, 10);
 
     if (!userId) {
       res.status(403).json({ message: 'No User Id defined' });
@@ -19,33 +20,36 @@ export async function checkProjectMembership(
     }
 
     // If the user is a SUPERADMIN, allow access to all tickets
-    if (userRole === 'SUPERADMIN') {
-      res.locals.userRole = userRole;
+    if (userGlobalRole === 'SUPERADMIN') {
+      res.locals.userGlobalRole = userGlobalRole;
       return next();
     }
 
-    const ticket = await prisma.ticket.findUnique({
-      where: { id: ticketIdNumber },
-      include: {
-        board: { include: { project: true } },
-      },
-    });
-
-    const projectId = ticket?.board.project.id;
-
-    const member = await prisma.projectMember.findFirst({
-      where: { projectId, userId: userId },
-    });
-
-    if (!member) {
-      res.status(403).json({ message: 'Forbidden: Not a Project Member' });
+    if (!userProjects) {
+      res.status(403).json({ message: 'User does not belong to any projects' });
       return;
     }
 
-    res.locals.projectMember = member;
+    const userProjectIds = userProjects.map(
+      (userProject) => userProject.projectId
+    );
+
+    if (!userProjectIds?.includes(requestedProjectId)) {
+      res.status(403).json({ message: 'Forbidden: Not a project member' });
+      return;
+    }
+
+    // The specific user information pertaining to requestedProjectId
+    const projectProfile = userProjects.find(
+      (project) => project.projectId === requestedProjectId
+    );
+
+    res.locals.userProjectDetails = projectProfile;
+
     next();
   } catch (error) {
     console.error('Auth Error:', error);
     res.status(500).json({ message: 'Server error during auth check' });
+    return;
   }
 }
