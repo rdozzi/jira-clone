68213f44f16b768b27c140a729f6b28926583369
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-09 14:04:56 -0700

    feat(backend): add SuperAdmin parameter and conditional check; omit use of Promise<void> for just void; omit async keyword

--- server/src/middleware/checkProjectMembership.ts
+++ server/src/middleware/checkProjectMembership.ts
@@ -6,56 +6,59 @@ type UserProject = {
   projectRole: ProjectRole;
 };
 
-export async function checkProjectMembership(
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> {
-  try {
-    const userId = res.locals.userInfo?.id;
-    const userGlobalRole = res.locals.userInfo?.globalRole;
-    const userProjects = res.locals.userProjects; // User project associations
+export function checkProjectMembership(options?: {
+  allowGlobalSuperAdmin?: boolean;
+}) {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      const userId = res.locals.userInfo?.id;
+      const userProjects = res.locals.userProjects; // User project associations
+      const isSuperAdmin: boolean = res.locals.isGlobalSuperAdmin;
 
-    const { projectId } = req.params; // For ProjectMember specific selections
-    const requestedProjectId = res.locals.projectId || parseInt(projectId, 10);
+      const { projectId } = req.params; // For ProjectMember specific selections
+      const requestedProjectId =
+        res.locals.projectId || parseInt(projectId, 10);
 
-    if (!userId) {
-      res.status(403).json({ message: 'No User Id defined' });
+      if (!userId) {
+        res.status(403).json({ message: 'No User Id defined' });
+        return;
+      }
+
+      // SuperAdmin bypass check
+
+      if (options?.allowGlobalSuperAdmin && isSuperAdmin) {
+        next();
+      }
+
+      if (!userProjects) {
+        res
+          .status(403)
+          .json({ message: 'User does not belong to any projects' });
+        return;
+      }
+
+      const userProjectIds: number[] = userProjects.map(
+        (userProject: UserProject) => userProject.projectId
+      );
+
+      if (!userProjectIds?.includes(requestedProjectId)) {
+        res.status(403).json({ message: 'Forbidden: Not a project member' });
+        return;
+      }
+
+      // The specific user information pertaining to requestedProjectId
+      const projectProfile = userProjects.find(
+        (userProject: UserProject) =>
+          userProject.projectId === requestedProjectId
+      );
+
+      res.locals.userProjectDetails = projectProfile;
+
+      next();
+    } catch (error) {
+      console.error('Auth Error:', error);
+      res.status(500).json({ message: 'Server error during auth check' });
       return;
     }
-
-    // If the user is a SUPERADMIN, allow access to all tickets
-    if (userGlobalRole === 'SUPERADMIN') {
-      res.locals.userGlobalRole = userGlobalRole;
-      res.locals.userId = userId;
-      return next();
-    }
-
-    if (!userProjects) {
-      res.status(403).json({ message: 'User does not belong to any projects' });
-      return;
-    }
-
-    const userProjectIds: number[] = userProjects.map(
-      (userProject: UserProject) => userProject.projectId
-    );
-
-    if (!userProjectIds?.includes(requestedProjectId)) {
-      res.status(403).json({ message: 'Forbidden: Not a project member' });
-      return;
-    }
-
-    // The specific user information pertaining to requestedProjectId
-    const projectProfile = userProjects.find(
-      (userProject: UserProject) => userProject.projectId === requestedProjectId
-    );
-
-    res.locals.userProjectDetails = projectProfile;
-
-    next();
-  } catch (error) {
-    console.error('Auth Error:', error);
-    res.status(500).json({ message: 'Server error during auth check' });
-    return;
-  }
+  };
 }
