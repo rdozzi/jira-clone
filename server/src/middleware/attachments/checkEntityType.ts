b14176b76f31dd9795e7f9f53d3a8dad471cd5c5
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-08 19:56:19 -0700

    feat(backend): create checkEntityType to provide projectId for attachment handling

--- server/src/middleware/attachments/checkEntityType.ts
+++ server/src/middleware/attachments/checkEntityType.ts
@@ -1,61 +1,74 @@
 import { Request, Response, NextFunction } from 'express';
 import { AttachmentEntityType } from '@prisma/client';
 import prisma from '../../lib/prisma';
+import { getProjectIdByTicket } from '../../utilities/getProjectIDByEntityUtilities/getProjectIdByTicket';
+import { getProjectIdByBoard } from '../../utilities/getProjectIDByEntityUtilities/getProjectByBoard';
+import { getProjectIdByComment } from '../../utilities/getProjectIDByEntityUtilities/getProjectByComment';
 
-// Route level middleware to get the project id for all entities except user. The project id will then allow for association and authorization checks at the project level.
+// Route level middleware to get the project id for all entities except user. The project id will then allow for association and authorization checks at the project level. User entities will have logic that isn't related to projects.
 
 export async function checkEntityType(
   req: Request,
   res: Response,
   next: NextFunction
-) {
+): Promise<void> {
   try {
-    const { attachmentId } = req.params;
-    const attachmentIdParsed = parseInt(attachmentId, 10);
+    const { entityId } = req.params;
+    const entityIdParsed = parseInt(entityId, 10);
 
-    if (isNaN(attachmentIdParsed)) {
-      return res.status(400).json({
+    if (isNaN(entityIdParsed)) {
+      res.status(400).json({
         message: 'Invalid attachment ID',
       });
+      return;
     }
 
     const attachment = await prisma.attachment.findUnique({
-      where: { id: attachmentIdParsed },
+      where: { id: entityIdParsed },
     });
 
     if (!attachment) {
-      return res.status(404).json({
+      res.status(404).json({
         message: 'Attachment not found',
       });
+      return;
     }
+
+    let projectId;
 
     switch (attachment.entityType) {
       case AttachmentEntityType.TICKET:
-        req.body.entityType = 'TICKET';
+        projectId = getProjectIdByTicket(entityIdParsed, prisma);
         break;
       case AttachmentEntityType.COMMENT:
-        req.body.entityType = 'COMMENT';
+        projectId = getProjectIdByComment(entityIdParsed, prisma);
         break;
       case AttachmentEntityType.BOARD:
-        req.body.entityType = 'BOARD';
+        projectId = getProjectIdByBoard(entityIdParsed, prisma);
         break;
       case AttachmentEntityType.PROJECT:
-        req.body.entityType = 'PROJECT';
+        // ProjectId is directly related to attachmentId
+        projectId = entityIdParsed;
         break;
       case AttachmentEntityType.USER:
-        req.body.entityType = 'USER';
+        res.locals.isUserAttachment = true; //No project related checks necessary
         break;
       default:
-        return res.status(400).json({
+        res.status(400).json({
           message: 'Unsupported entity type',
         });
+        return;
     }
+
+    res.locals.projectId = projectId; // This res.locals.projectId goes to checkProjectMembership
+
     next();
   } catch (error) {
     console.error('Error checking entity type: ', error);
-    return res.status(500).json({
+    res.status(500).json({
       message: 'Internal server error',
       error: (error as Error).message,
     });
+    return;
   }
 }
