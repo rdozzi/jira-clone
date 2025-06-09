8f590c5800141554c79057bbc4864f92a3cc9ca4
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-08 20:04:51 -0700

    chore(backend): add spaces for readability; changed Number to parseInt; added return statement to response.

--- server/src/controllers/attachmentControllers/getAllAttachments.ts
+++ server/src/controllers/attachmentControllers/getAllAttachments.ts
@@ -1,23 +1,27 @@
 import { Request, Response } from 'express';
 import { PrismaClient, AttachmentEntityType } from '@prisma/client';
+
 export async function getAllAttachments(
   req: Request,
   res: Response,
   prisma: PrismaClient
 ) {
   const { entityType, entityId } = req.body;
+
   try {
     const whereClause =
       entityType && entityId
         ? {
             entityType: entityType as AttachmentEntityType,
-            entityId: Number(entityId),
+            entityId: parseInt(entityId, 10),
           }
         : undefined;
+
     const attachments = await prisma.attachment.findMany({
       where: whereClause,
     });
-    res.status(200).json(attachments);
+
+    return res.status(200).json(attachments);
   } catch (error) {
     console.error('Error fetching attachments: ', error);
     res.status(500).json({ error: 'Failed to fetch attachments' });
