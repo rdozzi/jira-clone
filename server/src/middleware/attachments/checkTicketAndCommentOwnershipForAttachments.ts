1dc1d2d1a0e5f0847c769287946b9e59b35124ea
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-09 19:57:18 -0700

    feat(backend): create middleware to check for ownership of ticket or comment for upload and delete attachment operations

---
+++ server/src/middleware/attachments/checkTicketAndCommentOwnershipForAttachments.ts
@@ -0,0 +1,71 @@
+import { Request, Response, NextFunction } from 'express';
+import { AttachmentEntityType, GlobalRole } from '@prisma/client';
+import prisma from '../../lib/prisma';
+
+export async function checkTicketOrCommentOwnershipForAttachments(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    const entityType: AttachmentEntityType = res.locals.entityType;
+    const entityId: number = res.locals.entityId;
+    const userId: number = res.locals.userInfo.id;
+    const userGlobalRole = res.locals.userInfo.globalRole;
+
+    //If user is a SuperAdmin or the entityType is neither ticket or comment, go next
+    if (userGlobalRole === GlobalRole.SUPERADMIN) {
+      next();
+      return;
+    } else if (
+      entityType !== AttachmentEntityType.TICKET &&
+      entityType !== AttachmentEntityType.COMMENT
+    ) {
+      next();
+      return;
+    }
+    //If ticket, check if assigneeId === res.locals.userInfo.id, fail auth if not, next if so
+    if (entityType === AttachmentEntityType.TICKET) {
+      const ticket = await prisma.ticket.findFirst({
+        where: { id: entityId },
+      });
+
+      if (!ticket) {
+        res.status(404).json({ message: 'Ticket not found.' });
+        return;
+      }
+
+      if (ticket?.assigneeId !== userId) {
+        res.status(401).json({ message: 'You do not own this ticket' });
+        return;
+      }
+
+      return next();
+    }
+
+    //If comment, check if assigneeId === res.locals.userInfo.id, fail auth if not, next if so
+    else if (entityType === AttachmentEntityType.COMMENT) {
+      const comment = await prisma.comment.findFirst({
+        where: { id: entityId },
+      });
+
+      if (!comment) {
+        res.status(404).json({ message: 'Comment not found.' });
+        return;
+      }
+
+      if (comment?.authorId !== userId) {
+        res.status(401).json({ message: 'You do not own this comment' });
+        return;
+      }
+
+      return next();
+    }
+  } catch (error) {
+    console.error('Error checking for ticket or comment ownership:', error);
+    res.status(500).json({
+      message: `Error checking for ticket or comment ownership ${error}`,
+    });
+    return;
+  }
+}
