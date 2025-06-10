98287b8d6bbad1ca99d730fc0bc7d71201ba894a
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-09 20:00:24 -0700

    refactor(backend): update entityId implementation
    Updated entityId definition to except either req.params or req.body for entityId; Added validation checks for existence and appropriate value of entityId; Stored entityId as res.locals variable.

--- server/src/middleware/attachments/checkEntityType.ts
+++ server/src/middleware/attachments/checkEntityType.ts
@@ -13,7 +13,22 @@ export async function checkEntityType(
   next: NextFunction
 ): Promise<void> {
   try {
-    const { entityId } = req.params;
+    const entityId = req.params.entityId || req.body.entityId;
+
+    if (!entityId) {
+      res
+        .status(400)
+        .json({ message: 'Missing entityId in request parameters or body.' });
+      return;
+    }
+
+    res.locals.entityId = parseInt(entityId, 10);
+
+    if (isNaN(res.locals.entityId)) {
+      res.status(400).json({ message: 'Invalid entityId format.' });
+      return;
+    }
+
     const entityIdParsed = parseInt(entityId, 10);
 
     if (isNaN(entityIdParsed)) {
@@ -63,6 +78,8 @@ export async function checkEntityType(
     res.locals.projectId = projectId; // This res.locals.projectId goes to checkProjectMembership
 
     res.locals.entityType = attachment.entityType; // This res.locals.entityType goes to middleware to check for ticket or comment ownership
+
+    res.locals.entityId = entityIdParsed; // This res.locals.entityId goes to middleware to check for user ownership
 
     next();
   } catch (error) {
