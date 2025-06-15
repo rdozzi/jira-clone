daf924ae3678884325875e789c0a9577beea992a
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-15 13:38:08 -0700

    refactor(backend): remove checkIfGlobalSuperAdmin middleware function given update to checkProjectMembership and checkProjectRole

--- server/dist/router/projectRoutes.js
+++ server/dist/router/projectRoutes.js
@@ -10,7 +10,6 @@ const prisma_1 = __importDefault(require("../lib/prisma"));
 const projectController_1 = require("../controllers/projectController");
 // Middleware
 const authorizeGlobalRole_1 = require("../middleware/authAndLoadInfoMiddleware/authorizeGlobalRole");
-const checkIfGlobalSuperAdmin_1 = require("../middleware/checkIfGlobalSuperAdmin");
 const checkProjectMembership_1 = require("../middleware/checkProjectMembership");
 const checkProjectRole_1 = require("../middleware/checkProjectRole");
 const router = (0, express_1.Router)();
@@ -19,7 +18,7 @@ router.get('/projects', (0, authorizeGlobalRole_1.authorizeGlobalRole)(client_1.
     await (0, projectController_1.getAllProjects)(req, res, prisma_1.default);
 });
 // Get project by Id
-router.get('/projects/:projectId', checkIfGlobalSuperAdmin_1.checkIfGlobalSuperAdmin, (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.VIEWER, { allowGlobalSuperAdmin: true }), async (req, res) => {
+router.get('/projects/:projectId', (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.VIEWER, { allowGlobalSuperAdmin: true }), async (req, res) => {
     await (0, projectController_1.getProjectById)(req, res, prisma_1.default);
 });
 // Create project (Become a member of project)
@@ -27,11 +26,11 @@ router.post('/projects', (0, authorizeGlobalRole_1.authorizeGlobalRole)(client_1
     await (0, projectController_1.createProject)(req, res, next, prisma_1.default);
 });
 // Update project
-router.patch('/projects/:projectId', checkIfGlobalSuperAdmin_1.checkIfGlobalSuperAdmin, (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.ADMIN, { allowGlobalSuperAdmin: true }), async (req, res, next) => {
+router.patch('/projects/:projectId', (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.ADMIN, { allowGlobalSuperAdmin: true }), async (req, res, next) => {
     await (0, projectController_1.updateProject)(req, res, next, prisma_1.default);
 });
 // Delete project
-router.delete('/projects/:projectId', checkIfGlobalSuperAdmin_1.checkIfGlobalSuperAdmin, (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.ADMIN, { allowGlobalSuperAdmin: true }), async (req, res, next) => {
+router.delete('/projects/:projectId', (0, resolveProjectIdFromProject_1.resolveProjectIdFromProject)(), (0, checkProjectMembership_1.checkProjectMembership)({ allowGlobalSuperAdmin: true }), (0, checkProjectRole_1.checkProjectRole)(client_1.ProjectRole.ADMIN, { allowGlobalSuperAdmin: true }), async (req, res, next) => {
     await (0, projectController_1.deleteProject)(req, res, next, prisma_1.default);
 });
 exports.default = router;
