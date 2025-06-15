ec6e821575e03e498d13210a62701249f8e1d891
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-14 23:33:45 -0700

    feat(backend): update checkCommentOwnershp to include SuperAdmin check and others

---
+++ server/src/middleware/commentMiddleware/checkCommentOwnership.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import { GlobalRole } from '@prisma/client';
+import prisma from '../../lib/prisma';
+
+export function checkCommentOwnership(options?: {
+  allowGlobalSuperAdmin?: boolean;
+}) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const commentId = parseInt(req.params.commentId, 10);
+
+    if (!commentId || isNaN(commentId)) {
+      res.status(400).json({ message: 'Invalid comment Id' });
+      return;
+    }
+
+    const userId = res.locals.userInfo.id;
+    const globalRole = res.locals.userInfo.globalRole;
+
+    // SuperAdmin bypass check
+    if (
+      options?.allowGlobalSuperAdmin &&
+      globalRole === GlobalRole.SUPERADMIN
+    ) {
+      return next();
+    }
+
+    const comment = await prisma.comment.findUnique({
+      where: { id: commentId },
+    });
+
+    if (!comment || comment.authorId !== userId) {
+      res.status(403).json({ message: 'Forbidden: Not your comment' });
+      return;
+    }
+    next();
+  };
+}
