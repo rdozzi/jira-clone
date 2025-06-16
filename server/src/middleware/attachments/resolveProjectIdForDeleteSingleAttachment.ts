833c6f15e55d29cf3667dea3071ef84cef4dfa11
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-15 20:36:58 -0700

    feat(backend): create resolveProjectIdForDeleteSingleAttachment as middleware for the single deletion route

---
+++ server/src/middleware/attachments/resolveProjectIdForDeleteSingleAttachment.ts
@@ -0,0 +1,93 @@
+import { Request, Response, NextFunction } from 'express';
+import { AttachmentEntityType, PrismaClient, Attachment } from '@prisma/client';
+
+export function resolveProjectIdForDeleteSingleAttachment(
+  prisma: PrismaClient
+) {
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    // res.locals.attachment comes from validateAttachmentExistsAndStore
+    const attachment: Attachment = res.locals.attachment;
+
+    const entityId = attachment.entityId;
+    const entityType = attachment.entityType;
+
+    if (!entityId || isNaN(entityId)) {
+      res.status(404).json({ message: 'Entity Id is not valid' });
+      return;
+    }
+
+    if (!entityType) {
+      res.status(404).json({ message: 'Entity type is not valid' });
+      return;
+    }
+
+    try {
+      let projectId: number | undefined;
+
+      switch (entityType) {
+        case AttachmentEntityType.COMMENT: {
+          const comment = await prisma.comment.findFirst({
+            where: { id: entityId },
+            include: {
+              ticket: {
+                include: {
+                  board: { select: { projectId: true } },
+                },
+              },
+            },
+          });
+          projectId = comment?.ticket?.board?.projectId;
+          break;
+        }
+        case AttachmentEntityType.TICKET: {
+          const ticket = await prisma.ticket.findFirst({
+            where: { id: entityId },
+            include: {
+              board: { select: { projectId: true } },
+            },
+          });
+          projectId = ticket?.board?.projectId;
+          break;
+        }
+        case AttachmentEntityType.BOARD: {
+          const board = await prisma.board.findFirst({
+            where: { id: entityId },
+          });
+          projectId = board?.projectId;
+          break;
+        }
+        case AttachmentEntityType.PROJECT: {
+          projectId = entityId;
+          break;
+        }
+        default: {
+          res
+            .status(400)
+            .json({ message: `Unsupported entity type: ${entityType}` });
+          return;
+        }
+      }
+
+      if (projectId === undefined) {
+        res.status(404).json({ message: 'Project not found for given entity' });
+        return;
+      }
+
+      res.locals.projectId = projectId;
+      next();
+      return;
+    } catch (error) {
+      console.error(
+        `Error resolving projectId for ${entityType}:${entityId}`,
+        error
+      );
+      res.status(500).json({
+        message: 'Internal error resolving project association',
+      });
+    }
+  };
+}
