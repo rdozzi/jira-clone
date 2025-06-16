915f233a700cde4dc7e97100bb62c0be88fb17af
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-16 13:37:22 -0700

    feat(backend): remove next and prisma params; refactor const and return statements
    Removed references to next and prisma as this is terminal to the route and res.locals variables replaced the need to call the DB for information; Removed existence checks where performed upstream; Added return statements to all terminal blocks.

--- server/src/controllers/attachmentControllers/downloadController.ts
+++ server/src/controllers/attachmentControllers/downloadController.ts
@@ -1,44 +1,34 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import path from 'path';
 import { generateSignedCloudUrl } from '../../utilities/generateSignedCloudUrl';
-import { PrismaClient, Attachment } from '@prisma/client';
+import { Attachment } from '@prisma/client';
 import { buildLogEvent } from '../../services/buildLogEvent';
 
-export async function downloadSingleAttachment(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-  prisma: PrismaClient
-) {
-  const attachmentId = Number(req.params.id);
-  console.log(attachmentId);
+export async function downloadSingleAttachment(req: Request, res: Response) {
+  const { attachmentId } = req.params;
+  const attachmentIdParsed = parseInt(attachmentId, 10);
+  console.log(attachmentIdParsed);
   try {
-    const attachment = await prisma.attachment.findUnique({
-      where: { id: attachmentId },
-    });
-
-    if (!attachment) {
-      return res.status(404).json({ error: 'Attachment not found' });
-    }
+    const attachment: Attachment = res.locals.attachment;
 
     if (attachment.storageType === 'LOCAL') {
       const filePath = path.resolve(attachment.filePath || '');
       res.locals.logEvent = generatePayload(attachment);
       return res.download(filePath, attachment.fileName);
-      next();
     } else if (attachment.storageType === 'CLOUD') {
       const signedUrl = await generateSignedCloudUrl(attachment);
       res.locals.logEvent = generatePayload(attachment);
       res.status(201).json({
         message: `Signed URL generated successfully: ${signedUrl}`,
       });
-      next();
     } else {
       res.status(400).json({ error: 'Invalid storage type' });
+      return;
     }
   } catch (error) {
     console.error('Error downloading attachment: ', error);
     res.status(500).json({ error: 'Failed to download attachment' });
+    return;
   }
 }
 
