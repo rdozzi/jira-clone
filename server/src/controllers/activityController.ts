2bfde117ccec871b34ed64e7eb80341d28fe4735
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-17 18:49:23 -0700

    fix(backend): fix getLogByTicketId function after log structure change; insert return for all terminal responses
    Applied filtering clauses/criteria to metadata for function as ticketId will be conditionally rendered with new activityLog structure

--- server/src/controllers/activityController.ts
+++ server/src/controllers/activityController.ts
@@ -9,10 +9,10 @@ export async function getAllLogs(
 ) {
   try {
     const routes = await prisma.activityLog.findMany();
-    res.status(200).json(routes);
+    return res.status(200).json(routes);
   } catch (error) {
     console.error('Error fetching routes: ', error);
-    res.status(500).json({ error: 'Failed to fetch routes' });
+    return res.status(500).json({ error: 'Failed to fetch routes' });
   }
 }
 
@@ -24,24 +24,25 @@ export async function getLogbyTicketId(
 ) {
   try {
     const { ticketId } = req.params;
-    const ticketIdNumber = parseInt(ticketId, 10);
+    const ticketIdParsed = parseInt(ticketId, 10);
 
     if (!ticketId) {
       return res.status(400).json({ error: 'Ticket ID is required' });
     }
 
     const logs = await prisma.activityLog.findMany({
-      where: { ticketId: ticketIdNumber },
+      where: { metadata: { path: ['ticketId'], equals: ticketIdParsed } },
+      orderBy: { createdAt: 'desc' },
     });
 
     if (!logs) {
       return res.status(404).json({ error: 'No logs found' });
     }
 
-    res.status(200).json(logs);
+    return res.status(200).json(logs);
   } catch (error) {
     console.error('Error fetching logs by ticket ID: ', error);
-    res.status(500).json({ error: 'Failed to fetch logs' });
+    return res.status(500).json({ error: 'Failed to fetch logs' });
   }
 }
 
@@ -67,9 +68,9 @@ export async function getLogbyUserId(
       return res.status(404).json({ error: 'No logs found' });
     }
 
-    res.status(200).json(logs);
+    return res.status(200).json(logs);
   } catch (error) {
     console.error('Error fetching logs by user ID: ', error);
-    res.status(500).json({ error: 'Failed to fetch logs' });
+    return res.status(500).json({ error: 'Failed to fetch logs' });
   }
 }
