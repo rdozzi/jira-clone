dd6fd2aebeef2fd9f9c35b0e9af33e182336538d
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-18 18:52:27 -0700

    refactor(backend): refactor to implement userId from params, to add return statements to terminal conditionals, and remove RequestHandler type response.

--- server/src/middleware/authAndLoadInfoMiddleware/authorizeSelfOrAdmin.ts
+++ server/src/middleware/authAndLoadInfoMiddleware/authorizeSelfOrAdmin.ts
@@ -1,20 +1,24 @@
-import { RequestHandler, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { GlobalRole } from '@prisma/client';
 
-export function authorizeSelfOrAdmin(): RequestHandler {
+export function authorizeSelfOrAdmin() {
   return (req: Request, res: Response, next: NextFunction) => {
     const requestUserId = res.locals.userInfo.Id;
     const requestUserGlobalrole = res.locals.userInfo.globalRole;
-    const targetUserId = parseInt(req.params.id, 10);
+
+    const { userId } = req.params;
+    const userIdParsed = parseInt(userId, 10);
 
     if (
-      requestUserId === targetUserId ||
+      requestUserId === userIdParsed ||
       requestUserGlobalrole === GlobalRole.ADMIN ||
       requestUserGlobalrole === GlobalRole.SUPERADMIN
     ) {
       next();
+      return;
     } else {
       res.status(403).json({ message: 'Forbidden: insufficient privileges' });
+      return;
     }
   };
 }
