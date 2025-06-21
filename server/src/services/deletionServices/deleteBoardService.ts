134e29bc31463ac4e1cc7cbc3f7265afe92fa861
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-20 21:39:40 -0700

    feat(backend): create deleteBoardService to handle downstream dependencies recursively.

---
+++ server/src/services/deletionServices/deleteBoardService.ts
@@ -0,0 +1,46 @@
+import { Response } from 'express';
+import { AttachmentEntityType, Prisma } from '@prisma/client';
+import { deleteBoardDependencies } from './deleteBoardDependencies';
+import { buildLogEvent } from '../buildLogEvent';
+
+export async function deleteBoardService(
+  res: Response,
+  tx: Prisma.TransactionClient,
+  entityId: number,
+  userId: number
+) {
+  const boards = await tx.board.findMany({
+    where: { projectId: entityId },
+    select: { id: true, description: true, name: true },
+  });
+  if (boards.length > 0) {
+    const deletedBoards: typeof boards = JSON.parse(JSON.stringify(boards));
+    for (const boardObj of boards) {
+      await deleteBoardDependencies(
+        res,
+        tx,
+        AttachmentEntityType.BOARD,
+        boardObj.id,
+        userId
+      );
+      await tx.board.delete({ where: { id: boardObj.id } });
+    }
+    const logEventDeletedComments = deletedBoards.map((boardObj) => {
+      return buildLogEvent({
+        userId: userId,
+        actorType: 'USER',
+        action: 'DELETE_BOARD',
+        targetId: boardObj.id,
+        targetType: 'BOARD',
+        metadata: {
+          name: boardObj.name,
+          description: boardObj.description,
+        },
+      });
+    });
+
+    res.locals.logEvents = (res.locals.logEvents || []).concat(
+      logEventDeletedComments
+    );
+  }
+}
