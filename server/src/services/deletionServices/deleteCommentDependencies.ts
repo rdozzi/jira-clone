97d2da168010e109c289361d0f75cf1263359448
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-20 17:29:16 -0700

    feat(backend): rename deleteCommentCascade and modularize service
    Renamed deleteCommentCascade to deleteCommentDependencies; Modularized total deleteCommentDependencies into deleteCommentService.

---
+++ server/src/services/deletionServices/deleteCommentDependencies.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express';
+import { Prisma, AttachmentEntityType } from '@prisma/client';
+import { deleteAttachmentsService } from './deleteAttachmentsService';
+
+// Comments may have attachments associated with them. The attachment and associated record must be deleted.
+
+export async function deleteCommentDependencies(
+  res: Response,
+  tx: Prisma.TransactionClient,
+  attachmentEntity: AttachmentEntityType,
+  commentId: number,
+  userId: number
+) {
+  await deleteAttachmentsService(
+    res,
+    tx,
+    attachmentEntity, // COMMENT
+    commentId,
+    userId
+  );
+}
