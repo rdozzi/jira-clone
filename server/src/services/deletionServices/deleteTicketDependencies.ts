77cabd07b6d2ff7fac229645be617c428641d1fb
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-20 20:32:34 -0700

    feat(backend): update entityType and entityId argument reference; add comments
    Updated entityType and entityId to ticketEntity to ticketId respectively for semantic/contextual clarity. Added comments to document.

--- server/src/services/deletionServices/deleteTicketDependencies.ts
+++ server/src/services/deletionServices/deleteTicketDependencies.ts
@@ -7,16 +7,18 @@ import { deleteLabelDependencies } from './deleteLabelDependencies';
 export async function deleteTicketDependencies(
   res: Response,
   tx: Prisma.TransactionClient,
-  entityType: AttachmentEntityType, // TICKET
-  entityId: number, //ticketId
+  ticketEntity: AttachmentEntityType, // TICKET
+  ticketId: number, //ticketId
   userId: number
 ) {
-  //Delete related comments
-  deleteCommentService(res, tx, entityType, entityId, userId);
+  // Delete related comments
+  deleteCommentService(res, tx, ticketId, userId);
 
   // Delete TicketLabel Associations (Signature uses ticketId for entityId)
-  await deleteLabelDependencies(tx, null, entityId);
+  // Labels have no dependencies, no service is needed
+  await deleteLabelDependencies(tx, null, ticketId);
 
   // Delete related Attachments
-  await deleteAttachmentsService(res, tx, 'TICKET', entityId, userId);
+  // entityType for this service is the entity type sent from the service. In this case it should be "TICKET"
+  await deleteAttachmentsService(res, tx, ticketEntity, ticketId, userId);
 }
