eacfc9c712e9a3dccac92ae5235dd4aea8f56332
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-23 12:53:36 -0700

    fix(backend): add dotenv ref to prismaTestClient

--- server/src/lib/prismaTestClient.ts
+++ server/src/lib/prismaTestClient.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
 
 export const prismaTest = new PrismaClient({
   datasources: {
