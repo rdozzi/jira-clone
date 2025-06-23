7af9f57509be770b4b4e59f3a59fb623140fa4c2
Raphael Dozzi <rdozzi84@gmail.com>
2025-06-23 16:32:13 -0700

    refactor(backend): Remove reference to CustomRequest; implement res.locals variables to userId and userRole for ticket ownership check.

--- server/src/middleware/checkTicketOwnership.ts
+++ server/src/middleware/checkTicketOwnership.ts
@@ -1,14 +1,11 @@
 import { RequestHandler } from 'express';
-import { CustomRequest } from '../types/CustomRequest';
 import { PrismaClient } from '@prisma/client';
 
 export function checkTicketOwnership(prisma: PrismaClient): RequestHandler {
   return async (req, res, next) => {
-    const customReq = req as unknown as CustomRequest;
-
-    const userId = customReq.user?.id;
-    const userRole = customReq.user?.globalRole;
-    const ticketId = parseInt(customReq.params.id, 10);
+    const userId = res.locals.userInfo.id;
+    const userRole = res.locals.userInfo.globalRole;
+    const ticketId = parseInt(req.params.id, 10);
 
     // If the user is a SUPERADMIN, allow access to all comments
     if (userRole === 'SUPERADMIN') {
