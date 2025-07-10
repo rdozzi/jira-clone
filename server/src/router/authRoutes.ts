2262de56ab3f48326f08a534f1aa96bace90d086
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-09 22:08:04 -0700

    feat(backend): create rateLimiter file with loginRateLimiter and implement into auth route.

--- server/src/router/authRoutes.ts
+++ server/src/router/authRoutes.ts
@@ -1,12 +1,14 @@
 import { Router, Request, Response } from 'express';
 import prisma from '../lib/prisma';
 import { loginUser, logoutUser } from '../controllers/authController';
+import { loginRateLimiter } from '../middleware/rateLimiter';
 
 const router = Router();
 
 // Login user
 router.post(
   '/auth/login',
+  loginRateLimiter,
   async (req: Request, res: Response): Promise<void> => {
     await loginUser(req, res, prisma);
   }
