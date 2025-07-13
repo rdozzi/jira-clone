3efc45d6e60bc0452bc498a3f8ec149933e95596
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-13 09:04:25 -0700

    fix(tests): remove name and color formatting logic

--- server/src/utilities/testUtilities/createLabel.ts
+++ server/src/utilities/testUtilities/createLabel.ts
@@ -5,17 +5,6 @@ export function createLabel(
   name: string,
   color: string
 ) {
-  name =
-    typeof name === 'string'
-      ? name
-          .trim()
-          .replace(
-            /\w\S*/g,
-            (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
-          )
-      : '';
-
-  color = typeof color === 'string' ? color.toUpperCase() : '';
   const label = prismaTest.label.create({ data: { name: name, color: color } });
   return label;
 }
