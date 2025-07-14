67520abdec5dc12a0765e6947e7d32598f3c6e31
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-14 12:13:08 -0700

    refactor(backend): update authorizeSelfOrAdmin
    Combined Authorization check and self role change prevention check to apply to update user and avatar routes.

---
+++ server/src/middleware/userMiddleware/authorizeSelfOrAdminWithRoleCheck.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { GlobalRole } from '@prisma/client';
+
+export function authorizeSelfOrAdminWithRoleCheck() {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { id: requestUserId, globalRole: requestUserGlobalRole } =
+      res.locals.userInfo;
+    const { userId } = req.params;
+    const { globalRole } = req.body;
+
+    const userIdParsed = parseInt(userId, 10);
+    const isAdmin =
+      requestUserGlobalRole === GlobalRole.ADMIN ||
+      requestUserGlobalRole === GlobalRole.SUPERADMIN;
+    const isSelf = requestUserId === userIdParsed;
+
+    // Authorization check
+    if (!isSelf && !isAdmin) {
+      res.status(403).json({ message: 'Forbidden: insufficient privileges' });
+      return;
+    }
+
+    // Self role change prevention
+    if (isSelf && globalRole) {
+      res
+        .status(403)
+        .json({ error: 'Unauthorized to change your own global role.' });
+      return;
+    }
+
+    next();
+  };
+}
