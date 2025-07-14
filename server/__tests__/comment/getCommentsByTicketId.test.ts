43cb32d38fafae4bd36e8c2721c3afb25cb5c9a6
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-14 16:20:00 -0700

    test(tests): create tests for comment routes

---
+++ server/__tests__/comment/getCommentsByTicketId.test.ts
@@ -0,0 +1,56 @@
+import { describe, expect, afterAll, beforeAll, it } from '@jest/globals';
+import request from 'supertest';
+
+import { GlobalRole, User, Ticket, ProjectRole } from '@prisma/client';
+import { app } from '../../src/app';
+import { prismaTest } from '../../src/lib/prismaTestClient';
+
+import { resetTestDatabase } from '../../src/utilities/testUtilities/resetTestDatabase';
+import { createUserProfile } from '../../src/utilities/testUtilities/createUserProfile';
+import { createProject } from '../../src/utilities/testUtilities/createProject';
+import { createBoard } from '../../src/utilities/testUtilities/createBoard';
+import { createTicket } from '../../src/utilities/testUtilities/createTicket';
+import { createComment } from '../../src/utilities/testUtilities/createComment';
+import { createProjectMember } from '../../src/utilities/testUtilities/createProjectMember';
+import { generateJwtToken } from '../../src/utilities/testUtilities/generateJwtToken';
+
+describe('Get comments by ticket id', () => {
+  let token: string;
+  let user: User;
+  let ticket: Ticket;
+  const testDescription = 'getCommentsByTicketId';
+  beforeAll(async () => {
+    await prismaTest.$connect();
+    await resetTestDatabase();
+    user = await createUserProfile(
+      prismaTest,
+      testDescription,
+      GlobalRole.ADMIN
+    );
+    token = generateJwtToken(user.id, user.globalRole);
+    const project = await createProject(prismaTest, testDescription);
+    const board = await createBoard(prismaTest, testDescription, project.id);
+    ticket = await createTicket(prismaTest, testDescription, board.id, user.id);
+    await createComment(prismaTest, `${testDescription}_1`, ticket.id, user.id);
+    await createComment(prismaTest, `${testDescription}_2`, ticket.id, user.id);
+    await createComment(prismaTest, `${testDescription}_3`, ticket.id, user.id);
+    await createProjectMember(
+      prismaTest,
+      project.id,
+      user.id,
+      ProjectRole.VIEWER
+    );
+  });
+  afterAll(async () => {
+    await prismaTest.$disconnect();
+  });
+
+  it('should get all comments', async () => {
+    const res = await request(app)
+      .get(`/api/comments/${ticket.id}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toEqual('Comments were fetched successfully');
+    expect(res.body.data).toHaveLength(3);
+  });
+});
---
+++ server/__tests__/comment/updateComment.test.ts
@@ -0,0 +1,84 @@
+import { describe, expect, afterAll, beforeAll, it } from '@jest/globals';
+import request from 'supertest';
+
+import {
+  Board,
+  GlobalRole,
+  Project,
+  Ticket,
+  User,
+  ProjectRole,
+  Comment,
+} from '@prisma/client';
+import { app } from '../../src/app';
+import { prismaTest } from '../../src/lib/prismaTestClient';
+import { createUserProfile } from '../../src/utilities/testUtilities/createUserProfile';
+import { createProject } from '../../src/utilities/testUtilities/createProject';
+import { createBoard } from '../../src/utilities/testUtilities/createBoard';
+import { createTicket } from '../../src/utilities/testUtilities/createTicket';
+import { createComment } from '../../src/utilities/testUtilities/createComment';
+import { createProjectMember } from '../../src/utilities/testUtilities/createProjectMember';
+import { resetTestDatabase } from '../../src/utilities/testUtilities/resetTestDatabase';
+import { generateJwtToken } from '../../src/utilities/testUtilities/generateJwtToken';
+
+describe('Update Comment', () => {
+  let token: string;
+  let user: User;
+  let project: Project;
+  let board: Board;
+  let ticket: Ticket;
+  let comment: Comment;
+
+  const testDescription = 'updateComment';
+  beforeAll(async () => {
+    await prismaTest.$connect();
+    await resetTestDatabase();
+    user = await createUserProfile(
+      prismaTest,
+      testDescription,
+      GlobalRole.ADMIN
+    );
+    project = await createProject(prismaTest, testDescription);
+    board = await createBoard(prismaTest, testDescription, project.id);
+    ticket = await createTicket(prismaTest, testDescription, board.id, user.id);
+    comment = await createComment(
+      prismaTest,
+      testDescription,
+      ticket.id,
+      user.id
+    );
+    await createProjectMember(
+      prismaTest,
+      project.id,
+      user.id,
+      ProjectRole.USER
+    );
+    token = generateJwtToken(user.id, user.globalRole);
+  });
+  afterAll(async () => {
+    await prismaTest.$disconnect();
+  });
+
+  it("updates the user's information", async () => {
+    const res = await request(app)
+      .patch(`/api/comments/${comment.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        content: 'Updated content for test',
+      });
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        updatedComment: {
+          id: expect.any(Number),
+          ticketId: expect.any(Number),
+          authorId: expect.any(Number),
+          content: 'Updated content for test',
+          createdAt: expect.any(String),
+          updatedAt: expect.any(String),
+        },
+        message: expect.any(String),
+      })
+    );
+  });
+});
