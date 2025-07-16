0a32dcf5dcdf966b285df8e9216e236f2d0147d3
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-15 17:20:43 -0700

    refactor(backend): move checkTicketOwnership to ticketMiddleware folder

--- server/src/middleware/ticketMiddleware/checkTicketOwnership.ts
+++ server/src/middleware/ticketMiddleware/checkTicketOwnership.ts
@@ -5,7 +5,12 @@ export function checkTicketOwnership(prisma: PrismaClient): RequestHandler {
   return async (req, res, next) => {
     const userId = res.locals.userInfo.id;
     const userRole = res.locals.userInfo.globalRole;
-    const ticketId = parseInt(req.params.id, 10);
+    const { ticketId } = req.params;
+
+    if (!ticketId) {
+      res.status(400).json({ message: 'Invalid ticket id' });
+    }
+    const ticketIdParsed = parseInt(ticketId, 10);
 
     // If the user is a SUPERADMIN, allow access to all comments
     if (userRole === 'SUPERADMIN') {
@@ -13,7 +18,7 @@ export function checkTicketOwnership(prisma: PrismaClient): RequestHandler {
     }
 
     const ticket = await prisma.ticket.findUnique({
-      where: { id: ticketId },
+      where: { id: ticketIdParsed },
     });
 
     if (!ticket || ticket.assigneeId !== userId) {
