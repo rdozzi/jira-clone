a057098d4fae23c539437dd61f84a17ae6d9988e
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-16 17:52:15 -0700

    feat(backend): implement updates per tests
    Implemented boardId query logic into getAllBoards control function; Commented out getBoardById functions; Added await (debug) to deleteBoardDependencies function; Updated all res.status statement to have uniform output.

--- server/src/controllers/boardController.ts
+++ server/src/controllers/boardController.ts
@@ -11,8 +11,28 @@ export async function getAllBoards(
   prisma: PrismaClient
 ) {
   try {
-    const boards = await prisma.board.findMany();
-    res.status(200).json(boards);
+    const { boardId } = req.query;
+    const parsedBoardId =
+      typeof boardId === 'string' ? parseInt(boardId, 10) : undefined;
+
+    console.log(parsedBoardId);
+    const boards = await prisma.board.findMany({
+      where: { ...(parsedBoardId ? { id: parsedBoardId } : null) },
+    });
+
+    if (boards.length === 0) {
+      res.status(404).json({ message: 'No boards found' });
+      return;
+    }
+
+    console.log(boards.length);
+    res.status(200).json({
+      message:
+        boards.length > 1
+          ? 'Boards fetched successfully'
+          : 'Board fetched successfully',
+      data: boards,
+    });
     return;
   } catch (error) {
     console.error('Error fetching boards: ', error);
@@ -22,25 +42,26 @@ export async function getAllBoards(
 }
 
 // Get board by Id
-export async function getBoardById(
-  req: Request,
-  res: Response,
-  prisma: PrismaClient
-) {
-  const { boardId } = req.params;
-  const convertedBoardId = parseInt(boardId, 10);
-  try {
-    const board = await prisma.board.findUnique({
-      where: { id: convertedBoardId },
-    });
-    res.status(200).json(board);
-    return;
-  } catch (error) {
-    console.error('Error fetching board: ', error);
-    res.status(500).json({ error: 'Failed to fetch board' });
-    return;
-  }
-}
+// This function is deprecated. This doesn't really serve a purpose for UI functions.
+// export async function getBoardById(
+//   req: Request,
+//   res: Response,
+//   prisma: PrismaClient
+// ) {
+//   const { boardId } = req.params;
+//   const convertedBoardId = parseInt(boardId, 10);
+//   try {
+//     const board = await prisma.board.findUnique({
+//       where: { id: convertedBoardId },
+//     });
+//     res.status(200).json(board);
+//     return;
+//   } catch (error) {
+//     console.error('Error fetching board: ', error);
+//     res.status(500).json({ error: 'Failed to fetch board' });
+//     return;
+//   }
+// }
 
 // Get boards by Project Id
 export async function getBoardsByProjectId(
@@ -62,7 +83,9 @@ export async function getBoardsByProjectId(
       return;
     }
 
-    res.status(200).json(projectBoards);
+    res
+      .status(200)
+      .json({ message: 'Boards fetched successfully', data: projectBoards });
     return;
   } catch (error) {
     console.error(`Error fetching boards for project ${projectId}: `, error);
@@ -99,7 +122,9 @@ export async function createBoard(
       },
     });
 
-    res.status(200).json(board);
+    res
+      .status(200)
+      .json({ message: 'Board created successfully', data: board });
     return;
   } catch (error) {
     console.error('Error creating board: ', error);
@@ -150,7 +175,9 @@ export async function updateBoard(
         changes,
       },
     });
-    res.status(200).json(newBoard);
+    res
+      .status(200)
+      .json({ message: 'Board updated successfully', data: newBoard });
     return;
   } catch (error) {
     console.error('Error editing board: ', error);
@@ -185,7 +212,7 @@ export async function deleteBoard(
     }
 
     prisma.$transaction(async (tx) => {
-      deleteBoardDependencies(res, tx, 'BOARD', boardIdParsed, userId);
+      await deleteBoardDependencies(res, tx, 'BOARD', boardIdParsed, userId);
       await tx.board.delete({
         where: { id: boardIdParsed },
       });
@@ -202,7 +229,9 @@ export async function deleteBoard(
         description: oldBoard?.description,
       },
     });
-    res.status(200).json({ message: 'Board deleted successfully', oldBoard });
+    res
+      .status(200)
+      .json({ message: 'Board deleted successfully', data: oldBoard });
     return;
   } catch (error) {
     if (
