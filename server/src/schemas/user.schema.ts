48bfc9d0874eb1dd9453aec413762d7f55b0786e
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-19 22:03:06 -0700

    feat(backend): add schemas
    Added nameSchema, globalRoleSchema, createUserSchema, and updateUserSchema to file.

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,10 +1,30 @@
 import { z } from 'zod';
+import { GlobalRole } from '@prisma/client';
 
 export const emailSchema = z
   .email('A valid email is required')
   .trim()
   .min(5, 'Email must be 5 charcters long')
   .max(255, 'Email must be less than 255 characters');
+
+export const nameSchema = z
+  .string('A string is required')
+  .trim()
+  .min(3)
+  .max(128)
+  .refine((name) => /^[A-Za-z0-9 _'-]+$/.test(name), {
+    message: 'Name contains invalid characters',
+  })
+  .refine((name) => !/\s{2,}/.test(name), {
+    message: 'Name must not contain consecutive spaces',
+  })
+  .transform((name) =>
+    name
+      .split(' ')
+      .filter(Boolean)
+      .map((w) => w.charAt(0).toUpperCase() + w.substring(1).toLowerCase())
+      .join(' ')
+  );
 
 export const passwordSchema = z
   .string('A password is required')
@@ -18,3 +38,23 @@ export const passwordSchema = z
     /[^A-Za-z0-9]/,
     'Password must contain at least one special character'
   );
+
+export const globalRoleSchema = z
+  .string('A string is required')
+  .trim()
+  .toUpperCase()
+  .refine((val) => Object.values(GlobalRole).includes(val as GlobalRole), {
+    message: 'Invalid global role',
+  });
+
+export const createUserSchema = z
+  .object({
+    email: emailSchema,
+    firstName: nameSchema,
+    lastName: nameSchema,
+    password: passwordSchema,
+    globalRole: globalRoleSchema,
+  })
+  .strict();
+
+export const updateUserSchema = createUserSchema.partial().strict();
