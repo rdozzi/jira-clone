ba7dd0b3a66be3d991ef1d08e36d1afe542cb8c6
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-20 08:40:58 -0700

    refactor(backend): move all single type/entity schemas to base.schema file

--- server/src/schemas/auth.schema.ts
+++ server/src/schemas/auth.schema.ts
@@ -1,14 +1,5 @@
 import { z } from 'zod';
-
-const emailAuthSchema = z
-  .email('Invalid credentials')
-  .min(5, 'Invalid Credentials')
-  .max(255, 'Invalid Credentials');
-
-const passwordAuthSchema = z
-  .string('Invalid credentials')
-  .min(1, 'Invalid credentials')
-  .max(128, 'Invalid credentials');
+import { emailAuthSchema, passwordAuthSchema } from './base.schema';
 
 export const authCredentialCheckSchema = z.object({
   email: emailAuthSchema,
--- server/src/schemas/label.schema.ts
+++ server/src/schemas/label.schema.ts
@@ -1,34 +1,14 @@
 import { z } from 'zod';
-
-const nameSchema = z
-  .string('Name is required')
-  .trim()
-  .min(1, 'Name cannot be empty')
-  .max(25, 'Name must be less than 25 characters')
-  .pipe(
-    z.transform((str) =>
-      str.replace(
-        /\w\S*/g,
-        (txt) => txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase()
-      )
-    )
-  );
-
-const colorSchema = z
-  .string()
-  .regex(
-    /^#[0-9A-F]{6}$/,
-    'Color must be a valid 6-digit hex code (e.g., #AABBCC).'
-  )
-  .trim()
-  .toUpperCase();
+import { labelNameSchema, labelColorSchema } from './base.schema';
 
 export const labelCreateSchema = z.object({
-  name: nameSchema,
-  color: colorSchema,
+  name: labelNameSchema,
+  color: labelColorSchema,
 });
 
-export const labelUpdateSchema = z.object({
-  name: nameSchema.optional(),
-  color: colorSchema.optional(),
-});
+export const labelUpdateSchema = z
+  .object({
+    name: labelNameSchema,
+    color: labelColorSchema,
+  })
+  .partial();
