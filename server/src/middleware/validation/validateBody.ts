983ff57cce495fbc620f72865751fd777c613e08
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-19 21:58:18 -0700

    feat(backend): create validateBody that accepts schema paramter

---
+++ server/src/middleware/validation/validateBody.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express';
+import { ZodType, ZodError, z } from 'zod';
+
+export function validateBody(schema: ZodType) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result!.success) {
+      const error: ZodError = result!.error;
+      const prettifiedError = z.prettifyError(error);
+      res.status(400).json({
+        error: 'Validation failed',
+        message: prettifiedError,
+      });
+      return;
+    }
+
+    res.locals.validatedBody = result!.data;
+    next();
+  };
+}
