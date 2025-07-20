f37aae43cc6e690816432b3c530129e11ae95b23
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-19 19:23:25 -0700

    feat(backend): create file for getUserByIdOrEmail with queries

---
+++ server/src/middleware/validation/validateQuery.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { numberIdSchema } from '../../schemas/numberId.schema';
+import { emailQuerySchema } from '../../schemas/emailQuery.schema';
+import { z, ZodError } from 'zod';
+
+export function validateQuery(req: Request, res: Response, next: NextFunction) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const entitykey = Object.keys(req.query)[0];
+
+  let result;
+
+  const queryProperty = req.query[entitykey];
+
+  if (typeof queryProperty === 'string' && emailPattern.test(queryProperty)) {
+    result = emailQuerySchema.safeParse(req.query[entitykey]);
+  } else {
+    result = numberIdSchema.safeParse(req.query[entitykey]);
+  }
+
+  if (!result!.success) {
+    const error: ZodError = result!.error;
+    const prettifiedError = z.prettifyError(error);
+    res.status(400).json({
+      error: 'Validation failed',
+      message: prettifiedError,
+    });
+    return;
+  }
+
+  res.locals.validatedParam = { query: entitykey, data: result!.data };
+  console.log('validateQuery', res.locals.validatedParam);
+  next();
+}
