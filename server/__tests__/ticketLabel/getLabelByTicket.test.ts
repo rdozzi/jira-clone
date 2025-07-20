152aa2b081dacea23701962b7e6252aede2b28f0
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-20 11:45:32 -0700

    test(tests): debug and add test to getLabelByTicket
    Debugged payload response mismatch for get (single) pair; Added another test with entity dependencies for multiple fetch

--- server/__tests__/ticketLabel/getLabelByTicket.test.ts
+++ server/__tests__/ticketLabel/getLabelByTicket.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, afterAll, beforeAll, it } from '@jest/globals';
 import request from 'supertest';
-import waitForExpect from 'wait-for-expect';
 
 import { GlobalRole, Ticket, User, Label, ProjectRole } from '@prisma/client';
 import { app } from '../../src/app';
@@ -18,8 +17,10 @@ import { createTicketLabel } from '../../src/utilities/testUtilities/createTicke
 describe('Get Label by Ticket Id', () => {
   let token: string;
   let user: User;
-  let ticket: Ticket;
-  let label: Label;
+  let ticket1: Ticket;
+  let ticket2: Ticket;
+  let label1: Label;
+  let label2: Label;
   const testDescription = 'Get Label by Ticket Id';
   beforeAll(async () => {
     await prismaTest.$connect();
@@ -32,9 +33,23 @@ describe('Get Label by Ticket Id', () => {
     token = generateJwtToken(user.id, user.globalRole);
     const project = await createProject(prismaTest, testDescription, user.id);
     const board = await createBoard(prismaTest, testDescription, project.id);
-    ticket = await createTicket(prismaTest, testDescription, board.id, user.id);
-    label = await createLabel(prismaTest, 'label', '#FF0000');
-    await createTicketLabel(prismaTest, ticket.id, label.id);
+    ticket1 = await createTicket(
+      prismaTest,
+      testDescription,
+      board.id,
+      user.id
+    );
+    ticket2 = await createTicket(
+      prismaTest,
+      testDescription,
+      board.id,
+      user.id
+    );
+    label1 = await createLabel(prismaTest, 'label', '#FF0000');
+    label2 = await createLabel(prismaTest, 'label', '#C3E73C');
+    await createTicketLabel(prismaTest, ticket1.id, label1.id);
+    await createTicketLabel(prismaTest, ticket2.id, label1.id);
+    await createTicketLabel(prismaTest, ticket2.id, label2.id);
     await createProjectMember(
       prismaTest,
       project.id,
@@ -46,25 +61,29 @@ describe('Get Label by Ticket Id', () => {
     await prismaTest.$disconnect();
   });
 
-  it('should delete a label from a ticket', async () => {
+  it('should get a label from a ticket', async () => {
     const res = await request(app)
-      .get(`/api/ticket-labels/${ticket.id}`)
+      .get(`/api/ticket-labels/${ticket1.id}`)
       .set('Authorization', `Bearer ${token}`);
     expect(res.status).toBe(200);
-    expect(res.body).toEqual({
-      removedTicketLabel: {
-        ticketId: ticket.id,
-        labelId: label.id,
-      },
-      message: 'Label remove successful',
-    });
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        message: 'Label fetch successful (Note: Might be empty)',
+        ticketLabelPair: expect.arrayContaining([
+          { ticketId: expect.any(Number), labelId: expect.any(Number) },
+        ]),
+      })
+    );
+  });
 
-    await waitForExpect(async () => {
-      const ticketLabelRecord = await prismaTest.ticketLabel.findUnique({
-        where: { ticketId_labelId: { ticketId: ticket.id, labelId: label.id } },
-      });
-
-      expect(ticketLabelRecord).toBeNull();
-    });
+  it('should get many labels from a ticket', async () => {
+    const res = await request(app)
+      .get(`/api/ticket-labels/${ticket2.id}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(res.status).toBe(200);
+    expect(res.body.message).toEqual(
+      'Label fetch successful (Note: Might be empty)'
+    );
+    expect(res.body.ticketLabelPair).toHaveLength(2);
   });
 });
