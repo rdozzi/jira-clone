3fc3913cfd1644be3303c8327282d67042e0b59f
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-20 18:40:17 -0700

    feat(backend): add single-entity schemas for ticket specific payload object properties; update logic for bad words list
    Tokenized words in word bodies (comment contents, titles, descriptions, etc...) to fail tests that contain bad words per leo-profanity

--- server/src/schemas/base.schema.ts
+++ server/src/schemas/base.schema.ts
@@ -1,5 +1,12 @@
 import { z } from 'zod';
-import { GlobalRole, ProjectRole, AttachmentEntityType } from '@prisma/client';
+import {
+  GlobalRole,
+  ProjectRole,
+  AttachmentEntityType,
+  Status,
+  Priority,
+  Type,
+} from '@prisma/client';
 import LeoProfanity from 'leo-profanity';
 
 const filter = LeoProfanity;
@@ -24,10 +31,12 @@ export const commentContentSchema = z
   .max(500, 'Comment cannot exceed 500 characters including spaces')
   .transform((val) => val.replace(/\s+/g, ' ').trim())
   .refine(
-    (val) =>
-      !badWordsList.some((word) =>
-        val.toLowerCase().includes(word.toLowerCase())
-      ),
+    (val) => {
+      const tokens = val.toLowerCase().split(/\s+/);
+      return !tokens.some((token) =>
+        badWordsList.some((word) => token === word.toLowerCase())
+      );
+    },
     { error: 'Comment contains prohibited language.' }
   );
 
@@ -53,6 +62,12 @@ export const globalRoleSchema = z
   .toUpperCase()
   .refine((val) => Object.values(GlobalRole).includes(val as GlobalRole), {
     message: 'Invalid global role',
+  });
+
+export const isoDateStringSchema = z
+  .string()
+  .refine((val) => !isNaN(Date.parse(val)), {
+    message: 'Invalid date format',
   });
 
 export const labelColorSchema = z
@@ -127,6 +142,62 @@ export const projectRoleSchema = z
   .trim()
   .toUpperCase()
   .refine((val) => Object.values(ProjectRole).includes(val as ProjectRole), {
+    message: 'Invalid project role',
+  });
+
+export const ticketStatusSchema = z
+  .string('A string is required')
+  .trim()
+  .toUpperCase()
+  .refine((val) => Object.values(Status).includes(val as Status), {
+    message: 'Invalid project role',
+  });
+
+export const ticketDescriptionSchema = z
+  .string('Ticket description is required')
+  .trim()
+  .min(10, 'Ticket description must be at least 10 characters')
+  .max(3000, 'Ticket description cannot exceed 3000 characters')
+  .transform((val) => val.replace(/\s+/g, ' ').trim())
+  .refine(
+    (val) => {
+      const tokens = val.toLowerCase().split(/\s+/);
+      return !tokens.some((token) =>
+        badWordsList.some((word) => token === word.toLowerCase())
+      );
+    },
+    { error: 'Comment contains prohibited language.' }
+  );
+
+export const ticketTitleSchema = z
+  .string('Ticket title is required')
+  .trim()
+  .min(3, 'Ticket title must be at least 3 characters')
+  .max(150, 'Ticket title cannot exceed 150 characters')
+  .transform((val) => val.replace(/\s+/g, ' ').trim())
+  .refine(
+    (val) => {
+      const tokens = val.toLowerCase().split(/\s+/);
+      return !tokens.some((token) =>
+        badWordsList.some((word) => token === word.toLowerCase())
+      );
+    },
+    { error: 'Comment contains prohibited language.' }
+  );
+
+export const ticketPrioritySchema = z
+  .string('A string is required')
+  .trim()
+  .toUpperCase()
+  .refine((val) => Object.values(Priority).includes(val as Priority), {
+    message: 'Invalid project role',
+  });
+
+export const ticketTypeSchema = z
+  .string('A string is required')
+  .trim()
+  .toUpperCase()
+  .refine((val) => Object.values(Type).includes(val as Type), {
     message: 'Invalid project role',
   });
 
