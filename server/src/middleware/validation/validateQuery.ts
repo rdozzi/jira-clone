47ea635b3350e86c3b99e21273533aa3e3cc5dce
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-20 18:49:52 -0700

    feat(backend): update function to process single, multiple queries or no queries

--- server/src/middleware/validation/validateQuery.ts
+++ server/src/middleware/validation/validateQuery.ts
@@ -4,28 +4,39 @@ import { z, ZodError } from 'zod';
 
 export function validateQuery(req: Request, res: Response, next: NextFunction) {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const entitykey = Object.keys(req.query)[0];
+  const entityKeys = Object.keys(req.query);
 
-  let result;
-
-  const queryProperty = req.query[entitykey];
-
-  if (typeof queryProperty === 'string' && emailPattern.test(queryProperty)) {
-    result = emailQuerySchema.safeParse(req.query[entitykey]);
-  } else {
-    result = numberIdSchema.safeParse(req.query[entitykey]);
-  }
-
-  if (!result!.success) {
-    const error: ZodError = result!.error;
-    const prettifiedError = z.prettifyError(error);
-    res.status(400).json({
-      error: 'Validation failed',
-      message: prettifiedError,
-    });
+  if (entityKeys.length === 0) {
+    res.locals.validatedQuery = entityKeys;
+    next();
     return;
   }
 
-  res.locals.validatedQuery = { query: entitykey, data: result!.data };
+  const queryData: { [key: string]: unknown } = {};
+
+  let result;
+
+  for (const entityKey of entityKeys) {
+    const queryProperty = req.query[entityKey];
+
+    if (typeof queryProperty === 'string' && emailPattern.test(queryProperty)) {
+      result = emailQuerySchema.safeParse(req.query[entityKey]);
+    } else {
+      result = numberIdSchema.safeParse(req.query[entityKey]);
+    }
+
+    if (!result.success) {
+      const error: ZodError = result.error;
+      const prettifiedError = z.prettifyError(error);
+      res.status(400).json({
+        error: 'Validation failed',
+        message: prettifiedError,
+      });
+      return;
+    }
+    queryData[entityKey] = result.data;
+  }
+
+  res.locals.validatedQuery = queryData;
   next();
 }
