3879a88dc12bcb25a0333354386a06de02846785
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-22 16:11:15 -0700

    feat(backend): update route to include validateBody for input payload

--- server/src/router/setupRoutes.ts
+++ server/src/router/setupRoutes.ts
@@ -1,13 +1,16 @@
 import { Request, Response, Router } from 'express';
 import prisma from '../lib/prisma';
-import { seedSuperAdmin } from '../controllers/setupController';
+import { seedOrganizationAndSuperAdmin } from '../controllers/setupController';
+import { validateBody } from '../middleware/validation/validateBody';
+import { seedSuperAdminSchema } from '../schemas/setup.schema';
 
 const router = Router();
 
 router.post(
   '/setup/seedSuperAdmin',
+  validateBody(seedSuperAdminSchema),
   async (req: Request, res: Response): Promise<void> => {
-    await seedSuperAdmin(req, res, prisma);
+    await seedOrganizationAndSuperAdmin(req, res, prisma);
   }
 );
 
