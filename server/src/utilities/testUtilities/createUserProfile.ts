726b4d3c11246fc9e12718331a4dd9a18f4a8461
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-25 18:18:13 -0700

    fix(tests): Update file to include OrganizationRole.
    Integrated OrganizationRole type with organizationId in to user create payload for testing

--- server/src/utilities/testUtilities/createUserProfile.ts
+++ server/src/utilities/testUtilities/createUserProfile.ts
@@ -1,13 +1,13 @@
-import { GlobalRole, PrismaClient } from '@prisma/client';
+import { OrganizationRole, PrismaClient, GlobalRole } from '@prisma/client';
 import { hashPassword } from '../password';
 
 export async function createUserProfile(
   prismaTest: PrismaClient,
   testDescription: string,
-  globalRole: GlobalRole,
+  organizationRole: OrganizationRole,
   organizationId: number
 ) {
-  const email = `global_${globalRole}_${testDescription}@example.com`;
+  const email = `global_${organizationRole}_${testDescription}@example.com`;
   const user = await prismaTest.user.findUnique({
     where: { email: email },
   });
@@ -17,11 +17,12 @@ export async function createUserProfile(
   } else {
     const user = await prismaTest.user.create({
       data: {
-        email: `global_${globalRole}_${testDescription}@example.com`,
+        email: `global_${organizationRole}_${testDescription}@example.com`,
         firstName: `User_${testDescription}_firstName`,
         lastName: `User_${testDescription}_lastName`,
         passwordHash: await hashPassword('seedPassword123'),
-        globalRole: globalRole,
+        globalRole: GlobalRole.USER,
+        organizationRole: organizationRole,
         organizationId: organizationId,
       },
     });
