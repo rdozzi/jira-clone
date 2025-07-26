f6f272ba760ac232f3c3a8aeb878b3a5dd0ebdfe
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-25 18:14:03 -0700

    fix(backend): update utility to reference organization and Organization role instead of Global

--- server/src/lib/roles.ts
+++ server/src/lib/roles.ts
@@ -1,17 +1,17 @@
 import { ProjectRole, OrganizationRole } from '@prisma/client';
 
-export const globalRoleHierarchy = Object.values(OrganizationRole);
+export const organizationRoleHierarchy = Object.values(OrganizationRole);
 export const projectRoleHierarchy = Object.values(ProjectRole);
 
-export type OrganizationAppRole = (typeof globalRoleHierarchy)[number];
+export type OrganizationAppRole = (typeof organizationRoleHierarchy)[number];
 export type ProjectAppRole = (typeof projectRoleHierarchy)[number];
 
-export function hasRequiredGlobalRole(
+export function hasRequiredOrganizationRole(
   userRole: OrganizationAppRole,
   requiredRole: OrganizationAppRole
 ): boolean {
-  const userIndex = globalRoleHierarchy.indexOf(userRole);
-  const requiredIndex = globalRoleHierarchy.indexOf(requiredRole);
+  const userIndex = organizationRoleHierarchy.indexOf(userRole);
+  const requiredIndex = organizationRoleHierarchy.indexOf(requiredRole);
 
   return userIndex >= requiredIndex;
 }
