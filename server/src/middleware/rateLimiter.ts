ace553318b4566cc38f8d20c869ab939ef317cc8
Raphael Dozzi <rdozzi84@gmail.com>
2025-07-25 20:21:54 -0700

    feat(backend): update rate limiter feature
    Updated the rateLimiter file to include both globalRateLimiter, updated from rateLimit, and routeRateLimiter; Removed rate limiter reference from authRoutes and added to the global routes; Updated the public routes (auth/setup) to be more specific to avoid rate limiting application.

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -1,10 +1,21 @@
 import rateLimit from 'express-rate-limit';
 
-export const loginRateLimiter = rateLimit({
+export const globalRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 5,
+  limit: 100,
   message: {
     error: 'Too many login attempts from this IP address. Try again later.',
+  },
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+export const routeRateLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  limit: 3,
+  message: {
+    error:
+      'Too many creation attempts from this IP address. Try again in one hour.',
   },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
