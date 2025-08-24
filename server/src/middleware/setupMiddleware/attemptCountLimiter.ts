e0abc1e8f82a9807a0c20ec1c0cfd3d9fec3ab7d
Raphael Dozzi <rdozzi84@gmail.com>
2025-08-23 18:39:27 -0700

    fix(backend): update expire time for attemptKey and blockedEmailKey for setup route
    The expiration time for a user is set to end of day each day rather than 24 hour periods from when they made their first attempt.

--- server/src/middleware/setupMiddleware/attemptCountLimiter.ts
+++ server/src/middleware/setupMiddleware/attemptCountLimiter.ts
@@ -6,13 +6,17 @@ export function attemptCountLimiter() {
   return async (req: Request, res: Response, next: NextFunction) => {
     await connectRedis();
 
+    const todayEnd = new Date();
+    todayEnd.setUTCHours(23, 59, 59, 999);
+    const EXPIRE_TIME = Math.floor(todayEnd.getTime() / 1000);
+
     const timeInSeconds = 60 * 60 * 24; // 1 day
     const { email } = res.locals.validatedBody;
 
     const attemptKey = `attemptPayload:${email}`;
     const blockedEmailKey = `blockedEmail:${email}`;
-    await redisClient.expire(attemptKey, timeInSeconds, 'NX');
-    await redisClient.expire(blockedEmailKey, timeInSeconds, 'NX');
+    await redisClient.expireAt(attemptKey, EXPIRE_TIME, 'NX');
+    await redisClient.expireAt(blockedEmailKey, EXPIRE_TIME, 'NX');
     await redisClient.incr(attemptKey);
 
     if (Number(await redisClient.get(attemptKey)) >= 5) {
