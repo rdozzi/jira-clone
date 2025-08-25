0c6edf0bfd70ef6c0ea77a8ab27e43a1b5fb45f7
Raphael Dozzi <rdozzi84@gmail.com>
2025-08-25 12:15:31 -0700

    feat(backend): implement route and controller for delete banned email; add user info
    Created deletBannedEmail function with deleteResourceService and route reference; updated createBannedEmail to include user infor for ActivityLog.

--- server/src/controllers/bannedEmailController.ts
+++ server/src/controllers/bannedEmailController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { buildLogEvent } from '../services/buildLogEvent';
 import { createResourceService } from '../services/organizationUsageServices/createResourceService';
+import { deleteResourceService } from '../services/organizationUsageServices/deleteResourceService';
 
 // Get all banned email records
 export async function getAllBannedEmails(
@@ -54,6 +55,7 @@ export async function createBannedEmail(
   prisma: PrismaClient
 ) {
   try {
+    const user = res.locals.userInfo;
     const { email, reason } = res.locals.validatedBody;
     const organizationId = res.locals.userInfo.organizationId;
     const resourceType = res.locals.resourceType;
@@ -80,9 +82,9 @@ export async function createBannedEmail(
 
     // Log the event
     res.locals.logEvent = buildLogEvent({
-      userId: null,
+      userId: user.id,
       actorType: 'USER',
-      action: 'BAN_USER',
+      action: 'BAN_USER_EMAIL',
       targetId: null,
       targetType: 'USER',
       organizationId: organizationId,
@@ -101,5 +103,58 @@ export async function createBannedEmail(
   } catch (error) {
     console.error('Error creating banned email: ', error);
     return res.status(500).json({ error: 'Failed to create banned email' });
+  }
+}
+
+// Function deletes the record from the banned email list, effectively un-banning the email
+export async function deleteBannedEmail(
+  req: Request,
+  res: Response,
+  prisma: PrismaClient
+) {
+  try {
+    const user = res.locals.userInfo;
+    const organizationId = res.locals.userInfo.organizationId;
+    const emailId = res.locals.validatedParam;
+
+    const oldEmail = await prisma.bannedEmail.findUnique({
+      where: { id: emailId, organizationId: organizationId },
+    });
+
+    if (!oldEmail) {
+      return res.status(404).json({ error: 'Banned email not found' });
+    }
+
+    await deleteResourceService(
+      prisma,
+      organizationId,
+      async (tx) =>
+        await tx.bannedEmail.delete({
+          where: { id: emailId, organizationId: organizationId },
+        })
+    );
+
+    // Log the event
+    res.locals.logEvent = buildLogEvent({
+      userId: user.id,
+      actorType: 'USER',
+      action: 'DELETE_BANNED_EMAIL',
+      targetId: null,
+      targetType: 'USER',
+      organizationId: organizationId,
+      metadata: {
+        message: 'Previous ban information',
+        reason: `${oldEmail.reason}`,
+        email: `${oldEmail.email}`,
+        createdAt: `${oldEmail.createdAt}`,
+      },
+    });
+    res
+      .status(200)
+      .json({ message: 'Banned email deleted successfully', data: oldEmail });
+    return;
+  } catch (error) {
+    console.error('Error deleting banned email: ', error);
+    return res.status(500).json({ error: 'Failed to delete banned email' });
   }
 }
