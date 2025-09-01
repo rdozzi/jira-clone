4c4a0e51496184c06b114101efddb51f9ad2f022
Raphael Dozzi <rdozzi84@gmail.com>
2025-08-31 18:09:03 -0700

    fix(backend): update prisma to be a true singleton instance

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'test' ? [] : ['query', 'error', 'warn'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
