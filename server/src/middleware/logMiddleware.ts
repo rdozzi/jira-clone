83359e69a4f4e8951639c4d748f408343390b741
Raphael Dozzi <rdozzi84@gmail.com>
2025-08-31 18:30:57 -0700

    fix(backend): implemente setImmediate and update conditional logic
    Attempted to remedy res.on recursion feedback problem with setImmediate to no avail. Updating the global middle mechanism regardless; Updated conditional logic for logEvent to capture. This will be changed.

--- server/src/middleware/logMiddleware.ts
+++ server/src/middleware/logMiddleware.ts
@@ -5,22 +5,26 @@ import { createActivityLog } from '../services/logService';
 // This middleware will log the activity after the response is sent
 export function globalLogMiddleware() {
   return function (req: Request, res: Response, next: NextFunction) {
-    res.on('finish', async () => {
-      const logEvent = res.locals.logEvent;
-      const logEvents = res.locals.logEvents;
-      if (res.statusCode < 400) {
-        try {
-          if (logEvents && Array.isArray(logEvents)) {
-            await Promise.all(
-              logEvents.map((logEvent) => createActivityLog(logEvent))
-            );
-          } else if (logEvent) {
-            await createActivityLog(logEvent);
+    res.on('finish', () => {
+      setImmediate(async () => {
+        const logEvent = res.locals.logEvent;
+        const logEvents = res.locals.logEvents;
+        if (res.statusCode < 400) {
+          try {
+            if (logEvents && Array.isArray(logEvents)) {
+              await Promise.all(
+                logEvents.map((logEvent) => createActivityLog(logEvent))
+              );
+            }
+            if (logEvent) {
+              await createActivityLog(logEvent);
+            }
+            return;
+          } catch (error) {
+            console.error('Error creating activity log(s): ', error);
           }
-        } catch (error) {
-          console.error('Error creating activity log(s): ', error);
         }
-      }
+      });
     });
 
     next();
