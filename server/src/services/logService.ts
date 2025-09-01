2942621d37107c50901a87db757fb77f4d390caa
Raphael Dozzi <rdozzi84@gmail.com>
2025-09-01 13:53:43 -0700

    fix(backend): create logBus Event Listener PubSub process
    Created logBus event listener service to act as internal PubSub for ActivityLog operations. This was to totally decouple activity log creation from the JS event loop; Added function call to app.ts for global application access.

--- server/src/services/logService.ts
+++ server/src/services/logService.ts
@@ -1,7 +1,21 @@
 import { ActivityLog } from '@prisma/client';
 import { LogEventPayload } from '../types/logEventPayload';
 import prisma from '../lib/prisma';
-// import { activityLogCounter } from './organizationUsageServices/activityLogCounter';
+import { logBus } from '../lib/logBus';
+
+export function initLogService() {
+  logBus.on('activityLog', async (activityLogs: LogEventPayload[]) => {
+    try {
+      setImmediate(async () => {
+        await Promise.all(
+          activityLogs.map((activityLog) => createActivityLog(activityLog))
+        );
+      });
+    } catch (error) {
+      console.error('Could not access createActivityLog:', error);
+    }
+  });
+}
 
 export async function createActivityLog({
   userId,
