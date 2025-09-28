48c899eaabe70ab0d388e08259eab8c7bcdd5e4b
Raphael Dozzi <rdozzi84@gmail.com>
2025-09-28 10:38:38 -0700

    fix(frontend): insert types into function arguments and logic to rectify TS errors

--- client/src/utilities/getUpdatedFields.ts
+++ client/src/utilities/getUpdatedFields.ts
@@ -1,52 +1,23 @@
-// Function used to obtain the updated fields of a ticket form.
+import { Tickets } from '../types/Tickets';
 
-// interface Ticket {
-//   assigneeId: number;
-//   boardId: number;
-//   createdAt: string;
-//   description?: string;
-//   dueDate?: string; // ISO 8601 format e.g., "2025-02-16T23:59:59Z"
-//   id: number;
-//   priority: 'LOW' | 'MEDIUM' | 'HIGH';
-//   reporterId: number;
-//   status: 'BACKLOG' | 'IN_PROGRESS' | 'CLOSED'; // Enum-like behavior for status
-//   title: string;
-//   type: 'BUG' | 'TASK' | 'STORY';
-//   updatedAt: string;
-// }
+export function getUpdatedFields(ticketDbEntry: Tickets, updatedValues: any) {
+  const updatedFields: any = {};
+  for (const key of Object.keys(updatedValues) as (keyof Tickets)[]) {
+    const original = ticketDbEntry[key];
+    const updated = updatedValues[key];
 
-// interface UpdatedTicketFields {
-//   title?: string;
-//   description?: string;
-//   boardId?: number;
-//   reporterId?: number;
-//   assigneeId?: number | null;
-//   dueDate?: string;
-//   status?: 'BACKLOG' | 'IN_PROGRESS' | 'CLOSED';
-//   priority?: 'LOW' | 'MEDIUM' | 'HIGH';
-//   type: 'BUG' | 'TASK' | 'STORY';
-// }
+    if (key === 'dueDate') {
+      const dbDate =
+        original instanceof Date && original.toISOString().slice(0, 10);
+      const updatedDate =
+        updated instanceof Date && updated.toISOString().slice(0, 10);
 
-function getUpdatedFields(ticketDbEntry, updatedValues) {
-  console.log(ticketDbEntry);
-  console.log(updatedValues);
-  const updatedFields = {};
-  for (const [key] of Object.entries(updatedValues)) {
-    if (key in ticketDbEntry) {
-      if (key === 'dueDate') {
-        const dueDateDbEntry = ticketDbEntry.dueDate?.slice(0, 10);
-        const dueDateUpdatedValues = updatedValues.dueDate?.slice(0, 10);
-        if (dueDateDbEntry !== dueDateUpdatedValues) {
-          updatedFields[key] = updatedValues[key];
-        }
-      } else if (updatedValues[key] !== ticketDbEntry[key]) {
-        updatedFields[key] = updatedValues[key];
+      if (dbDate !== updatedDate) {
+        updatedFields[key] = updated as Tickets[typeof key];
       }
+    } else if (updated !== undefined && updated !== original) {
+      updatedFields[key] = updated as Tickets[typeof key];
     }
   }
-
-  console.log(updatedFields);
   return updatedFields;
 }
-
-export default getUpdatedFields;
