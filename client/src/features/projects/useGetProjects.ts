b8ef14bc9a4cc4d3946e83682df7a96a7b5e1e6f
Raphael Dozzi <rdozzi84@gmail.com>
2025-09-28 17:16:44 -0700

    feat(frontend): integrate conditional logic into useGetProjects to discern between Org Admin/SuperAdmin and User/Guest.

--- client/src/features/projects/useGetProjects.ts
+++ client/src/features/projects/useGetProjects.ts
@@ -1,14 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
-import { getProjects } from '../../services/apiProjects';
+import { getProjects, getProjectsByUserId } from '../../services/apiProjects';
+import { useGetUserSelf } from '../users/useGetUserSelf';
 
 export function useGetProjects() {
+  const { userSelf } = useGetUserSelf();
+  const isAdmin =
+    userSelf?.organizationRole === 'ADMIN' ||
+    userSelf?.organizationRole === 'SUPERADMIN';
+
   const {
     isLoading,
     data: projects,
     error,
   } = useQuery({
     queryKey: ['projects'],
-    queryFn: getProjects,
+    queryFn: () => {
+      if (isAdmin) {
+        return getProjects();
+      } else {
+        return getProjectsByUserId();
+      }
+    },
+    // queryFn: getProjects,
     staleTime: 0,
     gcTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
