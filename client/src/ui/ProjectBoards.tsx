464dd4ceab61bfa722fe0b5273cdac70a11a4b2b
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-05 12:10:46 -0700

    feat(frontend): implement getBoardsById into ProjectBoards component

--- client/src/ui/ProjectBoards.tsx
+++ client/src/ui/ProjectBoards.tsx
@@ -1,7 +1,32 @@
+// import { useState, useEffect } from 'react';
+import { useProjectInfo } from '../contexts/useProjectInfo';
+import { useGetBoardsByProjectId } from '../features/boards/useGetBoardsByProjectId';
+import { Spin } from 'antd';
+import { Boards } from '../types/Boards';
+
 function ProjectBoards() {
+  const { projectIdNumber } = useProjectInfo();
+  const { isLoading: isBoardLoading, boards } =
+    useGetBoardsByProjectId(projectIdNumber);
+
+  console.log(projectIdNumber);
+  console.log(boards);
+
   return (
     <>
       <div>ProjectBoards Page!</div>
+      {isBoardLoading ? (
+        <Spin />
+      ) : (
+        boards?.map((board: Boards) => (
+          <>
+            <div key={board.id}>
+              <div>Name: {board.name}</div>
+              <div>Description: {board.description}</div>
+            </div>
+          </>
+        ))
+      )}
     </>
   );
 }
