bd324d2d6e86d27802e869f5d52f450a38cd1f22
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-05 13:56:07 -0700

    feat(frontend): implement useGetUsersByProjectId into ProjectMembers

--- client/src/ui/ProjectMembers.tsx
+++ client/src/ui/ProjectMembers.tsx
@@ -1,7 +1,35 @@
+import { useProjectInfo } from '../contexts/useProjectInfo';
+import { useGetUsersByProjectId } from '../features/users/useGetUsersByProjectId';
+import { Spin } from 'antd';
+import { Users } from '../types/Users';
+
 function ProjectMembers() {
+  const { projectIdNumber } = useProjectInfo();
+  const { isLoadingUsers, projectUsers, error } =
+    useGetUsersByProjectId(projectIdNumber);
+
+  if (isLoadingUsers)
+    return (
+      <p>
+        <Spin />
+      </p>
+    );
+  if (error) return <p>Error Loading Projects!</p>;
+
   return (
     <>
       <div>Project Members!</div>
+      <div>
+        {projectUsers?.map((projectUser: Users) => (
+          <>
+            <div key={projectUser.id}>
+              <div>
+                Name: {projectUser.firstName} {projectUser.lastName}
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
     </>
   );
 }
