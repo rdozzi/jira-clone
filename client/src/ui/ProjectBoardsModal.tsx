fb861d5dd5659cd1a725a7110fcf1e162c39ff82
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-09 11:46:10 -0700

    feat(frontend): implement ProjectBoards Modal with api connections

---
+++ client/src/ui/ProjectBoardsModal.tsx
@@ -0,0 +1,189 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import { Modal, Form, Input, Select } from 'antd';
+import { useProjectInfo } from '../contexts/useProjectInfo';
+import { useGetBoardById } from '../features/boards/useGetBoardsById';
+import { useUpdateBoard } from '../features/boards/useUpdateBoard';
+import { getUpdatedBoardFields } from '../utilities/getUpdatedFields';
+import { Boards } from '../types/Boards';
+import { Projects } from '../types/Projects';
+
+import { useCreateBoard } from '../features/boards/useCreateBoard';
+
+export interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  record?: Boards;
+  mode: 'create' | 'viewEdit';
+}
+
+export interface Value {
+  project: number;
+  name: string;
+  descriptin: string;
+}
+
+function getOptions(projects?: Projects[]): { value: number; label: string }[] {
+  return (
+    projects?.map((project) => ({
+      value: project.id,
+      label: `${project.name}`,
+    })) || []
+  );
+}
+
+function ProjectBoardsModal({ isOpen, closeModal, record, mode }: ModalProps) {
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const { typedProjects, isProjectLoading } = useProjectInfo();
+  const { createBoard, isCreatingBoard } = useCreateBoard();
+  const { board } = useGetBoardById(record?.id);
+  const { updateBoard, isUpdatingBoard } = useUpdateBoard();
+
+  const [form] = Form.useForm();
+  const projectOptions = getOptions(typedProjects);
+
+  function handleOk() {
+    form.submit();
+  }
+
+  function handleCancel() {
+    closeModal();
+    form.resetFields();
+  }
+
+  function handleOnFinish(values: Value) {
+    if (mode === 'create') {
+      onFinishCreate(values);
+    } else {
+      onFinishEdit(values);
+    }
+  }
+
+  async function onFinishCreate(values: Value) {
+    try {
+      const { project, ...rest } = values;
+      const newValues = { projectId: project, ...rest };
+      createBoard(newValues);
+      setConfirmLoading(isCreatingBoard);
+      form.resetFields();
+      closeModal();
+    } catch (error) {
+      console.error('Error creating ticket: ', error);
+    } finally {
+      setConfirmLoading(false);
+    }
+  }
+
+  async function onFinishEdit(values: Value) {
+    try {
+      const { project, ...rest } = values;
+      const newValues = { projectId: project, ...rest };
+      const boardId = board.id;
+      const updatedFields = getUpdatedBoardFields(board, newValues);
+      updateBoard({ boardId, values: updatedFields });
+      setConfirmLoading(isUpdatingBoard);
+      form.resetFields();
+      closeModal();
+    } catch (error) {
+      console.error('Error updating ticket: ', error);
+      form.resetFields();
+      closeModal();
+    } finally {
+      setConfirmLoading(false);
+    }
+  }
+
+  return createPortal(
+    <Modal
+      title={
+        mode === 'create'
+          ? 'Enter Board Information Here'
+          : 'Edit Board Information'
+      }
+      onOk={handleOk}
+      confirmLoading={confirmLoading}
+      onCancel={handleCancel}
+      open={isOpen}
+      onClose={closeModal}
+      getContainer={false}
+      destroyOnClose={true}
+      mask={false}
+      okText={mode === 'create' ? 'Create' : 'Update'}
+    >
+      <Form
+        form={form}
+        name='basic'
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        onFinish={handleOnFinish}
+        autoComplete='off'
+        disabled={isProjectLoading}
+        initialValues={
+          mode === 'viewEdit'
+            ? {
+                project: record?.projectId,
+                name: record?.name,
+                description: record?.description,
+              }
+            : {}
+        }
+      >
+        <Form.Item
+          label='Project'
+          name='project'
+          rules={[
+            {
+              required: true,
+              message: 'Please provide a project!',
+            },
+          ]}
+        >
+          <Select
+            style={{ textAlign: 'left' }}
+            placeholder='Please select a project'
+          >
+            {projectOptions.map((options) => {
+              return (
+                <Select.Option key={options.value} value={options.value}>
+                  {options.label}
+                </Select.Option>
+              );
+            })}
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label='Name'
+          name='name'
+          rules={[
+            {
+              required: true,
+              message: 'Please provide a name',
+              max: 255,
+            },
+          ]}
+        >
+          <Input placeholder='Please enter a name' />
+        </Form.Item>
+
+        <Form.Item
+          label='Description'
+          name='description'
+          rules={[
+            {
+              required: true,
+              message: 'Please provide a description',
+              max: 255,
+            },
+          ]}
+        >
+          <Input placeholder='Please enter a description' />
+        </Form.Item>
+      </Form>
+    </Modal>,
+    document.body
+  );
+}
+
+export default ProjectBoardsModal;
