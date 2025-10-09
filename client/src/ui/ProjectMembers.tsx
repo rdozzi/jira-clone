e0cdd9533dcd4c5c8341a22221ff97f86dd844b6
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-09 15:00:54 -0700

    feat(frontend): implement table with Name, Role and option buttons for project members

--- client/src/ui/ProjectMembers.tsx
+++ client/src/ui/ProjectMembers.tsx
@@ -1,8 +1,26 @@
 import { useProjectMembers } from '../contexts/useProjectMembers';
-import { Spin } from 'antd';
+import { useProjectInfo } from '../contexts/useProjectInfo';
+import { Spin, Table, Button } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+import { ProjectMember } from '../types/ProjectMember';
+import { useModal } from '../contexts/useModal';
+
+import ProjectListItemButton from './ProjectListItemButton';
+import ProjectMembersModal from './ProjectMembersModal';
+
+const onChange: TableProps<ProjectMember>['onChange'] = (
+  pagination,
+  filters,
+  sorter,
+  extra
+) => console.log('params', pagination, filters, sorter, extra);
 
 function ProjectMembers() {
   const { projectMembers, isLoadingProjectMember, error } = useProjectMembers();
+  const { projectIdNumber } = useProjectInfo();
+  const { isOpen, openModal, closeModal, mode, modalProps } = useModal();
 
   if (isLoadingProjectMember)
     return (
@@ -10,20 +28,56 @@ function ProjectMembers() {
         <Spin />
       </div>
     );
-  if (error) return <div>Error Loading Projects!</div>;
+  if (error) return <div>Error Loading Project Members!</div>;
+
+  const columns: TableColumnsType<ProjectMember> = [
+    {
+      title: 'Name',
+      key: 'name',
+      render: (_: any, record: any) => `${record.firstName} ${record.lastName}`,
+      sorter: (a, b) =>
+        a.lastName.toLowerCase().localeCompare(b.lastName.toLowerCase()),
+    },
+    {
+      title: 'Role',
+      dataIndex: 'projectRole',
+      sorter: (a, b) => a.projectRole.localeCompare(b.projectRole),
+    },
+    {
+      title: '',
+      key: 'action',
+      align: 'center',
+      render: (record) => <ProjectListItemButton record={record} />,
+    },
+  ];
+
+  function handleCreate() {
+    openModal('create', {});
+  }
 
   return (
     <>
-      <div>Project Members!</div>
+      <Table<ProjectMember>
+        columns={columns}
+        dataSource={projectMembers}
+        rowKey='userId'
+        onChange={onChange}
+        loading={isLoadingProjectMember}
+        pagination={false}
+      />
       <div>
-        {projectMembers?.map((projectMember) => (
-          <div key={projectMember.userId}>
-            <div>
-              Name: {projectMember.firstName} {projectMember.lastName}
-            </div>
-          </div>
-        ))}
+        <Button onClick={handleCreate}>
+          <PlusOutlined /> Create
+        </Button>
       </div>
+      {mode === 'create' && (
+        <ProjectMembersModal
+          isOpen={isOpen}
+          closeModal={closeModal}
+          mode={mode}
+          {...modalProps}
+        />
+      )}
     </>
   );
 }
