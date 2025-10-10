104ec4c672bcf3f335a8a5d8831e00408b8e0de8
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-10 15:13:14 -0700

    feat(frontend): add id and projectId types to ProjectMember iterface; add PROJECT_ROLES definition to file

--- client/src/types/ProjectMember.ts
+++ client/src/types/ProjectMember.ts
@@ -1,8 +1,12 @@
 export interface ProjectMember {
+  id: number;
   userId: number;
+  projectId: number;
   projectRole: string;
   firstName: string;
   lastName: string;
   email: string;
   avatarUrl: string;
 }
+
+export const PROJECT_ROLES = ['VIEWER', 'USER', 'ADMIN'];
