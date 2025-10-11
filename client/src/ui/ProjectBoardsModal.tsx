ca2bc4a7106fa770a4ac74d2808856acbc40e3c2
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-10 19:08:38 -0700

    fix(frontend): update form name prop to be unique per AntD warnings; add null to mode property of ModalProps

--- client/src/ui/ProjectBoardsModal.tsx
+++ client/src/ui/ProjectBoardsModal.tsx
@@ -14,7 +14,7 @@ export interface ModalProps {
   isOpen: boolean;
   closeModal: () => void;
   record?: Boards;
-  mode: 'create' | 'viewEdit';
+  mode: 'create' | 'viewEdit' | null;
 }
 
 export interface Value {
@@ -112,7 +112,7 @@ function ProjectBoardsModal({ isOpen, closeModal, record, mode }: ModalProps) {
     >
       <Form
         form={form}
-        name='basic'
+        name='projectBoardForm'
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
