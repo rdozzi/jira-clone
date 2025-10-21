2bd9141bf9dfb557b4fdfa70837bb9664dcea5ec
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-20 21:42:16 -0700

    fix(frontend): add projectId to queryKey to make is unique (state was not changing with projectMembers key alone).

--- client/src/features/projectMember/useGetProjectMembers.ts
+++ client/src/features/projectMember/useGetProjectMembers.ts
@@ -1,16 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProjectMembers as apiGetProjectMembers } from '../../services/apiProjectMembers';
 
-export function useGetProjectMembers(projectId: number | null) {
+export function useGetProjectMembers(projectId: number | null | undefined) {
   const {
     data: projectMembers,
     isLoading: isLoadingProjectMember,
     error,
     refetch: refreshProjectMember,
   } = useQuery({
-    queryKey: ['projectMembers'],
+    queryKey: ['projectMembers', projectId],
     queryFn: () => apiGetProjectMembers(projectId as number),
-    enabled: projectId !== null,
+    enabled: !!projectId,
     staleTime: 0,
     gcTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
