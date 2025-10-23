372fea078ba1377a145cf7c76c9ec7f458408b6b
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-23 08:41:12 -0700

    feat(frontend): create api and query for getAttachments

---
+++ client/src/services/apiAttachments.ts
@@ -0,0 +1,28 @@
+import { getAuthToken } from '../lib/getAuthToken';
+import { EntityType } from '../types/Attachments';
+const token = getAuthToken();
+
+export async function getAllAttachments(
+  entityType: EntityType,
+  entityId: number
+) {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/attachments/${entityType}/${entityId}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    if (!res.ok) {
+      throw new Error('Failed to fetch attachments');
+    }
+    const { data } = await res.json();
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
