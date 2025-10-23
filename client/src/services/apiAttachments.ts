8ae98f981e8c7bf211a8a6766488dd0e8cf44453
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-23 11:07:17 -0700

    refactor(frontend): update getAttachments from getAllAttachments

--- client/src/services/apiAttachments.ts
+++ client/src/services/apiAttachments.ts
@@ -2,10 +2,7 @@ import { getAuthToken } from '../lib/getAuthToken';
 import { EntityType } from '../types/Attachments';
 const token = getAuthToken();
 
-export async function getAllAttachments(
-  entityType: EntityType,
-  entityId: number
-) {
+export async function getAttachments(entityType: EntityType, entityId: number) {
   try {
     const res = await fetch(
       `http://localhost:3000/api/attachments/${entityType}/${entityId}`,
