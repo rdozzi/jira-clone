fd001e0c56c5b11f609f90e9dc9136c21d5ec432
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-24 18:24:04 -0700

    feat(frontend): create Attachment interface

--- client/src/types/Attachments.ts
+++ client/src/types/Attachments.ts
@@ -1,1 +1,23 @@
-export type EntityType = 'PROJECT' | 'BOARD' | 'TICKET' | 'COMMENT' | 'USER';
+export type EntityType =
+  | 'PROJECT'
+  | 'BOARD'
+  | 'TICKET'
+  | 'COMMENT'
+  | 'USER'
+  | null;
+
+export interface Attachment {
+  id: number;
+  entityType: EntityType;
+  entityId: number;
+  fileName: string;
+  fileSize: number;
+  fileType: string;
+  fileUrl: string | null;
+  filePath: string | null;
+  uploadedBy: number;
+  organizationId: number;
+  createdAt: Date;
+  deletedAt: Date;
+  storageType: string;
+}
