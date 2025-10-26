8d244e3a2b04e00b3a7ad9d56973cdd6b125d988
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-25 21:14:04 -0700

    refactor(frontend): refactor AttachmentModalContext and Provider generalize state and function names similar to the TIcket modal

--- client/src/contexts/AttachmentModalProvider.tsx
+++ client/src/contexts/AttachmentModalProvider.tsx
@@ -11,29 +11,29 @@ type AttachmentModalState = {
 type ModalProviderProps = { children: React.ReactNode };
 
 export function AttachmentModalProvider({ children }: ModalProviderProps) {
-  const [attachmentModalState, setAttachmentModalState] =
-    useState<AttachmentModalState>({
-      isOpen: false,
-      mode: null as EntityType,
-      modalProps: {} as Record<string, unknown>,
-    });
+  const [modalState, setModalState] = useState<AttachmentModalState>({
+    isOpen: false,
+    mode: null as EntityType,
+    modalProps: {} as Record<string, unknown>,
+  });
 
-  function openAttachmentModal(mode: EntityType, modalProps = {}) {
-    setAttachmentModalState({ isOpen: true, mode, modalProps });
+  function openModal(mode: EntityType, modalProps = {}) {
+    console.log('AttachmentModalProvider', mode, modalProps);
+    setModalState({ isOpen: true, mode, modalProps });
   }
 
-  function closeAttachmentModal() {
-    setAttachmentModalState({ isOpen: false, mode: null, modalProps: {} });
+  function closeModal() {
+    setModalState({ isOpen: false, mode: null, modalProps: {} });
   }
 
   return (
     <AttachmentModalContext.Provider
       value={{
-        isAttachmentOpen: attachmentModalState.isOpen,
-        openAttachmentModal,
-        closeAttachmentModal,
-        modeAttachment: attachmentModalState.mode,
-        modalPropsAttachment: attachmentModalState.modalProps,
+        isOpen: modalState.isOpen,
+        openModal,
+        closeModal,
+        mode: modalState.mode,
+        modalProps: modalState.modalProps,
       }}
     >
       {children}
