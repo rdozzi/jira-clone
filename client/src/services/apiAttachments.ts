f9259ea884c67819d85f59909e1552f67157916b
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-27 17:37:05 -0700

    feat(frontend): create api endpoint and mutation call for uploadSingleAttachment

--- client/src/services/apiAttachments.ts
+++ client/src/services/apiAttachments.ts
@@ -25,3 +25,28 @@ export async function getAttachments(entityType: EntityType, entityId: number) {
     console.error(err);
   }
 }
+
+export async function uploadSingleAttachment(formData: FormData) {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/attachments/attachments/single`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: formData,
+      }
+    );
+    if (!res.ok) {
+      const errorText = await res.text();
+      console.error('Error:', res.status, res.statusText, errorText);
+      throw new Error('Failed to update board');
+    }
+    const { data } = await res.json();
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
