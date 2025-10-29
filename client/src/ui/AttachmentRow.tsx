22b343d14b316fb54f8ae2c33b91b9247d26988f
Raphael Dozzi <rdozzi84@gmail.com>
2025-10-28 17:45:52 -0700

    feat(frontend): integrate deletion logic into delete button for attachments

--- client/src/ui/AttachmentRow.tsx
+++ client/src/ui/AttachmentRow.tsx
@@ -1,6 +1,10 @@
 import { Attachment } from '../types/Attachments';
-import { Button, Flex } from 'antd';
+import { Button, Flex, Popconfirm, Tooltip } from 'antd';
 import { DownloadOutlined, DeleteOutlined } from '@ant-design/icons';
+import { useDeleteAttachment } from '../features/attachments/useDeleteAttachment';
+import { EntityType } from '../types/Attachments';
+import { ModalPropsWithRecord } from './AttachmentModal';
+import { useAttachmentModal } from '../contexts/useAttachmentModal';
 
 function editFilename(fileName: string) {
   const dashIndex = fileName.indexOf('-', 0);
@@ -9,11 +13,34 @@ function editFilename(fileName: string) {
 }
 
 function AttachmentRow({ attachment }: { attachment: Attachment }) {
+  const { mode, modalProps } = useAttachmentModal() as {
+    isOpen: boolean;
+    closeModal: () => void;
+    mode: EntityType;
+    modalProps: ModalPropsWithRecord;
+  };
+  const { deleteSingleAttachment, isDeletingAttachment } = useDeleteAttachment(
+    mode,
+    typeof modalProps?.id === 'number' ? modalProps.id : -1
+  );
+
   return (
     <Flex justify='flex-start' align='center' gap='small'>
       {editFilename(attachment.fileName)}
       <Button icon={<DownloadOutlined />} />
-      <Button icon={<DeleteOutlined />} />
+      <Popconfirm
+        title='Delete Attachments'
+        description='Are you sure you want to delete this attachment?'
+        onConfirm={() => deleteSingleAttachment(attachment.id)}
+        onCancel={() => console.log('User canceled deletion')}
+        placement='top'
+        okText='Yes'
+        cancelText='No'
+      >
+        <Tooltip title='Delete Attachment'>
+          <Button icon={<DeleteOutlined />} disabled={isDeletingAttachment} />
+        </Tooltip>
+      </Popconfirm>
     </Flex>
   );
 }
