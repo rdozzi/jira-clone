7db65ae4177030abd8379f9cb03d2ba250258173
Raphael Dozzi <rdozzi84@gmail.com>
2025-11-04 19:27:25 -0800

    feat(frontend): create api with type, mutation file, and helper function for user edit feature.

--- client/src/services/apiUsers.ts
+++ client/src/services/apiUsers.ts
@@ -1,4 +1,7 @@
 import { getAuthToken } from '../lib/getAuthToken';
+import { Users } from '../types/Users';
+
+type UpdatedUser = Partial<Users>;
 
 const token = getAuthToken();
 
@@ -54,6 +57,29 @@ export async function getUsersByProjectId(projectId: number) {
     );
     if (!res.ok) {
       throw new Error('Failed to fetch users');
+    }
+    const { data } = await res.json();
+    return data;
+  } catch (err: any | unknown) {
+    console.error(err);
+  }
+}
+
+export async function updateUser(userId: number, userInfo: UpdatedUser) {
+  try {
+    const res = await fetch(
+      `http://localhost:3000/api/users/${userId}/update`,
+      {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+      }
+    );
+    if (!res.ok) {
+      throw new Error('Failed to update User');
     }
     const { data } = await res.json();
     return data;
