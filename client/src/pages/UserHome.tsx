bb6f71cd098b3ab0275891983e19b722c60b4d90
Raphael Dozzi <rdozzi84@gmail.com>
2025-11-06 12:17:34 -0800

    feat(frontend): add user summaries to UserHome component

--- client/src/pages/UserHome.tsx
+++ client/src/pages/UserHome.tsx
@@ -1,5 +1,19 @@
+import { Row, Col, Card } from 'antd';
+
 function UserHome() {
-  return <div>This is my user homepage!</div>;
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={16}>
+        <Card title='My Active Tickets'>...</Card>
+      </Col>
+      <Col span={8}>
+        <Card title='Recent Activity'>...</Card>
+      </Col>
+      <Col span={16}>
+        <Card title='Upcoming Deadlines'>...</Card>
+      </Col>
+    </Row>
+  );
 }
 
 export default UserHome;
