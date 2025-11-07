3b595f67a13463c8a81a355580cb26a5f9787c4f
Raphael Dozzi <rdozzi84@gmail.com>
2025-11-06 16:18:14 -0800

    feat(frontend): add Overdue Tickets and Recent Activity table views

--- client/src/pages/UserHome.tsx
+++ client/src/pages/UserHome.tsx
@@ -3,14 +3,17 @@ import { Row, Col, Card } from 'antd';
 function UserHome() {
   return (
     <Row gutter={[16, 16]}>
-      <Col span={16}>
+      <Col span={12}>
         <Card title='My Active Tickets'>...</Card>
       </Col>
-      <Col span={8}>
-        <Card title='Recent Activity'>...</Card>
+      <Col span={12}>
+        <Card title='Overdue Tickets'>...</Card>
       </Col>
-      <Col span={16}>
+      <Col span={12}>
         <Card title='Upcoming Deadlines'>...</Card>
+      </Col>
+      <Col span={12}>
+        <Card title='Recent Activity'>...</Card>
       </Col>
     </Row>
   );
