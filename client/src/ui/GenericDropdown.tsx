2f7f2e03d0397b193e3fc09cffd07f6227d53710
Raphael Dozzi <rdozzi84@gmail.com>
2025-11-19 17:06:42 -0800

    feat(frontend): create GenericDropdown and SidebarActionButton components; update AppLayout with components

---
+++ client/src/ui/GenericDropdown.tsx
@@ -0,0 +1,46 @@
+import { Select } from 'antd';
+
+type DropdownItem = {
+  id: number;
+  name: string;
+};
+
+type GenericDropdownProps<T extends DropdownItem> = {
+  option: T | null;
+  options: T[];
+  setSelected: React.Dispatch<React.SetStateAction<T | null>>;
+  isSelectedLoading: boolean;
+  isProjectInfoView?: boolean;
+};
+
+function GenericDropdown<T extends DropdownItem>({
+  option,
+  options,
+  setSelected,
+  isSelectedLoading,
+  isProjectInfoView,
+}: GenericDropdownProps<T>) {
+  return (
+    <Select
+      style={{ width: '100%', textAlign: 'left' }}
+      value={option?.id}
+      loading={isSelectedLoading}
+      size={'middle'}
+      disabled={isProjectInfoView}
+      onChange={(id) => {
+        const selectedProject = options?.find((o) => o.id === id) || null;
+        setSelected(selectedProject);
+      }}
+    >
+      {options?.map((option) => {
+        return (
+          <Select.Option key={option.id} value={option.id}>
+            {option.name}
+          </Select.Option>
+        );
+      })}
+    </Select>
+  );
+}
+
+export default GenericDropdown;
