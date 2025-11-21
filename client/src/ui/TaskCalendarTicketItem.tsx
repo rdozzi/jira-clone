a39e2f1903b1fc81f7f91e4d23a3e11fa1ef0ea5
Raphael Dozzi <rdozzi84@gmail.com>
2025-11-21 13:32:24 -0800

    feat(ui): reformat ticket references on Calendar; add Popover and Descriptions components

--- client/src/ui/TaskCalendarTicketItem.tsx
+++ client/src/ui/TaskCalendarTicketItem.tsx
@@ -1,32 +1,74 @@
+import { Popover, Descriptions } from 'antd';
 import { Record } from './TicketListItemButton';
 import TicketListItemButton from './TicketListItemButton';
+import { useTheme } from '../contexts/useTheme';
 
 type CellRenderRecord = Record;
 
 // "record" is used in place of "ticket"
 function TaskCalendarCellRender({ record }: { record: CellRenderRecord }) {
+  const { modeTheme } = useTheme();
+
+  const content = (
+    <Descriptions
+      size='small'
+      column={1}
+      style={{
+        maxWidth: 250,
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+      }}
+    >
+      <Descriptions.Item label='Title'>
+        {record.title || 'NA'}
+      </Descriptions.Item>
+      <Descriptions.Item label='Description'>
+        {record.description || '—'}
+      </Descriptions.Item>
+      <Descriptions.Item label='Status'>{record.status}</Descriptions.Item>
+      <Descriptions.Item label='Due Date'>
+        {typeof record.dueDate === 'string'
+          ? record.dueDate
+          : record.dueDate.toLocaleString()}
+      </Descriptions.Item>
+    </Descriptions>
+  );
   return (
     <li
       key={record.id}
       style={{
         display: 'flex',
         alignItems: 'center',
+        padding: '2px 0',
       }}
     >
-      <span
-        style={{
-          margin: '1px',
-          padding: '2px 5px 2px 5px',
-          maxWidth: '100px',
-          textOverflow: 'ellipsis',
-          overflow: 'hidden',
-          whiteSpace: 'nowrap',
-          backgroundColor: '#ADD8E6',
-          textAlign: 'center',
-        }}
+      <Popover
+        title={'Ticket Info'}
+        content={content}
+        placement='right'
+        trigger='hover'
       >
-        {record.title}
-      </span>
+        <span
+          style={{
+            flex: 1,
+            padding: '2px 8px',
+            borderRadius: '5px',
+            backgroundColor:
+              modeTheme === 'light'
+                ? 'var(--ticket-bg, #DADADA)'
+                : 'var(--ticket-bg, #4c4c4c)',
+            fontSize: '12px',
+            lineHeight: 2,
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            whiteSpace: 'nowrap',
+          }}
+          title={record.title}
+        >
+          {record.title}
+        </span>
+      </Popover>
       <span style={{ marginLeft: '5px', marginRight: '5px' }}>
         <TicketListItemButton record={record} />
       </span>
