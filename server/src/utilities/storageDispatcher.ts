25d2db05f520c84f421e8794c68a93ce5144da81
Raphael Dozzi <rdozzi84@gmail.com>
2025-12-04 18:28:59 -0800

    feat(backend): update and integrate data for upload feature
    integrated uploadToCloud with organizationId, entityType, and entityId as arguments for key naming complexity.

--- server/src/utilities/storageDispatcher.ts
+++ server/src/utilities/storageDispatcher.ts
@@ -1,18 +1,21 @@
 import { getStorageType } from '../config/storage';
-import { saveToCloud } from './uploadToCloud';
+import { uploadToCloud } from './uploadToCloud';
 import { saveToLocal } from './saveToLocal';
 import { FileMetadata } from '../types/file';
 import { StorageType } from '@prisma/client';
 
 export async function storageDispatcher(
-  file: Express.Multer.File
+  file: Express.Multer.File,
+  organizationId: number,
+  entityType: string,
+  entityId: number
 ): Promise<FileMetadata> {
   const type: StorageType = getStorageType();
   switch (type) {
     case 'LOCAL':
       return saveToLocal(file);
     case 'CLOUD':
-      return saveToCloud(file);
+      return uploadToCloud(file, organizationId, entityType, entityId);
     default:
       throw new Error(`Invalid storage destination: ${type}`);
   }
