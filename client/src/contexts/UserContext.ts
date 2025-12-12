60cb5b9bcaaa51e0bab03aca67b04b24bdae130f
Raphael Dozzi <rdozzi84@gmail.com>
2025-12-12 08:52:44 -0800

    feat(frontend): implement derived orgId var into user context for query and mutations

--- client/src/contexts/UserContext.ts
+++ client/src/contexts/UserContext.ts
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
-// import { UseMutateFunction } from '@tanstack/react-query';
 import { Users } from '../types/Users';
 import { QueryObserverResult } from '@tanstack/react-query';
 
 interface UserContextType {
   userSelf: Users | null;
+  orgId: number | null;
   isLoadingUser: boolean;
   error: Error | null;
   refreshUser: () => Promise<QueryObserverResult<any, Error>>;
--- client/src/contexts/UserProvider.tsx
+++ client/src/contexts/UserProvider.tsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from 'react';
 import { UserContext } from './UserContext';
 import { useGetUserSelf } from '../features/users/useGetUserSelf';
 
 type UserProviderProps = { children: React.ReactNode };
 
-export const UserProviderContext = ({ children }: UserProviderProps) => {
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [orgId, setOrgId] = useState<number | null>(null);
   const { userSelf, isLoadingUser, error, refreshUser } = useGetUserSelf();
+
+  useEffect(() => {
+    if (userSelf?.organizationId) {
+      setOrgId(userSelf?.organizationId);
+    }
+  }, [userSelf?.organizationId]);
 
   return (
     <UserContext.Provider
-      value={{ userSelf, isLoadingUser, error, refreshUser }}
+      value={{ userSelf, orgId, isLoadingUser, error, refreshUser }}
     >
       {children}
     </UserContext.Provider>
