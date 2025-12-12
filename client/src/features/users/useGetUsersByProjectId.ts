e1ffd789fbe949327d38d048c5daf4967c2c82c6
Raphael Dozzi <rdozzi84@gmail.com>
2025-12-12 12:00:57 -0800

    feat(frontend): update User hooks with orgId and isAuthenticated for useGetUserSelf

--- client/src/features/users/useGetUsersByProjectId.ts
+++ client/src/features/users/useGetUsersByProjectId.ts
@@ -1,18 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import { getUsersByProjectId as apiGetUsersByProjectId } from '../../services/apiUsers';
+import { useUser } from '../../contexts/useUser';
 
 export function useGetUsersByProjectId(projectId: number) {
+  const { orgId } = useUser();
   const {
     isLoading: isLoadingUsers,
     data: projectUsers,
     error,
     refetch: refreshUsers,
   } = useQuery({
-    queryKey: ['projectUsers'],
+    queryKey: ['projectUsers', orgId],
     queryFn: () => apiGetUsersByProjectId(projectId),
     staleTime: 0,
     gcTime: 1000 * 60 * 10,
     refetchOnWindowFocus: false,
+    enabled: !!orgId,
   });
 
   return { isLoadingUsers, projectUsers, error, refreshUsers };
