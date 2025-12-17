2e011ff75eb26646e4f799aedb8560cb7ddaefc6
Raphael Dozzi <rdozzi84@gmail.com>
2025-12-16 19:46:14 -0800

    refactor(frontend): refactor App file
    Broke up the App file to separate concers into three different components: AppProviders, AppRoutes, and AuthBoundaryProviders.

---
+++ client/src/app/AppRoutes.tsx
@@ -0,0 +1,69 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { AuthBoundaryProviders } from './AuthBoundaryProviders';
+
+import LoginPage from '../pages/LoginPage';
+import ProtectedRoute from '../router/ProtectedRoute';
+import UserHome from '../pages/UserHome';
+import NotFoundPage from '../ui/NotFoundPage';
+import AppLayout from '../ui/AppLayout';
+import TicketList from '../pages/TicketList';
+import TaskCalendar from '../pages/TaskCalendar';
+import TaskBoard from '../pages/TaskBoard';
+import ProjectOverview from '../ui/ProjectOverview';
+import ProjectMembers from '../ui/ProjectMembers';
+import ProjectBoards from '../ui/ProjectBoards';
+import ProjectViewAll from '../ui/ProjectViewAll';
+import ProjectInfoLayout from '../ui/ProjectInfoLayout';
+import ModalLayer from '../ui/ModalLayer';
+import UserProfile from '../ui/UserProfile';
+import PublicRoute from '../utilities/PublicRoute';
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      {/* -- Public Routes -- */}
+      <Route
+        path='/login'
+        element={
+          <PublicRoute>
+            <LoginPage />
+          </PublicRoute>
+        }
+      />
+
+      {/* Protected Routes */}
+      <Route
+        element={
+          <ProtectedRoute>
+            <AuthBoundaryProviders>
+              <AppLayout>{<></>}</AppLayout>
+              <ModalLayer />
+            </AuthBoundaryProviders>
+          </ProtectedRoute>
+        }
+      >
+        {/* User Homepage; Renders depending on User Profile */}
+        <Route path='/user-homepage' element={<UserHome />} />
+        <Route path='user-profile' element={<UserProfile />} />
+
+        {/* Tickets Section */}
+        <Route path='/tickets'>
+          <Route path='ticketlist' element={<TicketList />} />
+          <Route path='taskboard' element={<TaskBoard />} />
+          <Route path='calendar' element={<TaskCalendar />} />
+        </Route>
+
+        <Route path='/projects/:projectId/*' element={<ProjectInfoLayout />}>
+          <Route path='overview' element={<ProjectOverview />} />
+          <Route path='boards' element={<ProjectBoards />} />
+          <Route path='members' element={<ProjectMembers />} />
+        </Route>
+        <Route path='/projects/view-all' element={<ProjectViewAll />} />
+      </Route>
+
+      {/* 404 Not Found */}
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  );
+}
