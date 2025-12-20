29c77eab4202d284b19c2057d107db4c4898875f
Raphael Dozzi <rdozzi84@gmail.com>
2025-12-19 17:33:49 -0800

    fix(frontend): Change Record to TicketRecord in types/Tickets; Add TicketModalPayload to add as generic to ModalState

--- client/src/types/Tickets.ts
+++ client/src/types/Tickets.ts
@@ -2,6 +2,26 @@ export type Priority = 'LOW' | 'MEDIUM' | 'HIGH';
 export type Status = 'BACKLOG' | 'IN_PROGRESS' | 'DONE';
 export type Type = 'BUG' | 'TASK' | 'STORY';
 
+export interface TicketRecord {
+  assignee: { firstName: string; lastName: string };
+  assigneeId: number;
+  boardId: number;
+  createdAt: Date;
+  description: string;
+  dueDate: Date | string;
+  id: number;
+  priority: Priority;
+  reporterId: number;
+  status: string;
+  title: string;
+  type: string;
+  updatedAt: Date;
+}
+
+export type TicketModalPayload = {
+  record?: TicketRecord;
+};
+
 export interface Tickets {
   assignee: { firstName: string; lastName: string };
   assigneeId: number;
