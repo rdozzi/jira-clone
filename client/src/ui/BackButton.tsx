70673f21eda2cf6a95d3d474763364fa408e2859
Raphael Dozzi <rdozzi84@gmail.com>
2026-02-27 15:56:42 -0800

    feat(frontend): Added Popover for user guidance

--- client/src/ui/BackButton.tsx
+++ client/src/ui/BackButton.tsx
@@ -1,23 +1,25 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Popover } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 
 function BackButton({ children }: { children?: React.ReactNode }) {
   const navigate = useNavigate();
   return (
-    <Button
-      type='text'
-      icon={<ArrowLeftOutlined />}
-      onClick={() => {
-        navigate(-1);
-      }}
-      style={{
-        fontSize: '20px',
-        transition: 'transform 0.3s ease-in-out',
-      }}
-    >
-      {children}
-    </Button>
+    <Popover placement='right' content={'Go back'}>
+      <Button
+        type='text'
+        icon={<ArrowLeftOutlined />}
+        onClick={() => {
+          navigate(-1);
+        }}
+        style={{
+          fontSize: '20px',
+          transition: 'transform 0.3s ease-in-out',
+        }}
+      >
+        {children}
+      </Button>
+    </Popover>
   );
 }
 
