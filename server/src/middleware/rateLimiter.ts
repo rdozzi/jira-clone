677edef8a61b1f791f820dbcdbe359a81bcbe1ea
Raphael Dozzi <rdozzi84@gmail.com>
2026-03-06 11:40:17 -0800

    fix(backend): reinstall express-rate-limit; update rateLimiter

--- server/src/middleware/rateLimiter.ts
+++ server/src/middleware/rateLimiter.ts
@@ -8,20 +8,17 @@ const ROUTE_LIMIT = isProd ? 300 : 10000;
 export const globalRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   limit: GLOBAL_LIMIT,
-  message: {
-    error: 'Too many login attempts from this IP address. Try again later.',
-  },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // message: Too many requests, please try again later.
+  // statusCode: 429
 });
 
 export const routeRateLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
   limit: ROUTE_LIMIT,
-  message: {
-    error:
-      'Too many creation attempts from this IP address. Try again in one hour.',
-  },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // message: Too many requests, please try again later.
+  // statusCode: 429
 });
